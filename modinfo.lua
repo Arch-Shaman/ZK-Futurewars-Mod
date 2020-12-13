f055d81c1e32047274d6441058efc3e68fad29c5
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2020-12-13 00:04:38 -0500

    v0.0.0.4

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Zero-K',
   description='Zero-K',
   shortname='ZK',
-  version='Future Wars v0.0.0.3',
+  version='Future Wars v0.0.0.4',
   mutator='1',
   game='Zero-K',
   shortGame='ZK',
