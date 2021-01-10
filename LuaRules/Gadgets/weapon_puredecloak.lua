a40559101ceba361e5990cc5f3e62ac12090b483
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-01-10 13:04:00 -0500

    Add Pure Decloak damage

---
+++ LuaRules/Gadgets/weapon_puredecloak.lua
@@ -0,0 +1,29 @@
+function gadget:GetInfo()
+	return {
+		name      = "Pure Decloak",
+		desc      = "Blocks cloaking for X seconds.",
+		author    = "Shaman",
+		date      = "January 10, 2021",
+		license   = "CC-0",
+		layer     = 5,
+		enabled   = true,
+	}
+end
+
+if not gadgetHandler:IsSyncedCode() then -- no unsynced nonsense
+	return
+end
+
+local config = {}
+for i = 1, #WeaponDefs do
+	if WeaponDefs[i].customParams and WeaponDefs[i].customParams.puredecloaktime then
+		config[i] = WeaponDefs[i].customParams.puredecloaktime
+	end
+end
+
+function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
+	if config[weaponDefID] then
+		GG.BlockCloakForUnit(unitID, config[weaponDefID])
+		return 0, 0
+	end
+end
