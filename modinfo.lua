fda40e070008ecfe092373cac8204ea968945460
HTMLPhoton <61311312+HTMLPhoton@users.noreply.github.com>
2021-01-17 14:59:09 +1100

    Create modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,12 @@
+return {
+	name='Future Wars ',
+	description='Zero-K',
+	shortname='ZKFW',
+	version='v0.16.8',
+	mutator='1',
+	game='Zero-K',
+	shortGame='ZK',
+	modtype=1,
+	depend = {
+		[[rapid://zk:stable]],
+	
