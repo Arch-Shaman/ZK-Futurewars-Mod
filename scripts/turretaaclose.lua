927f19c8d6333ca826d73f2f0d1efbe14861bcc3
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-01-18 22:05:19 -0500

    Touch up Duster's OKP

--- scripts/turretaaclose.lua
+++ scripts/turretaaclose.lua
@@ -156,7 +156,7 @@ function script.BlockShot(num, targetID)
 		return false
 	end
 	if gun[shot].loaded then
-		return GG.Script.OverkillPreventionCheck(unitID, targetID, 300.1, 430, 26, 0.1, true)
+		return GG.Script.OverkillPreventionCheck(unitID, targetID, 300.1, 1000, 52, 0, true)
 	end
 	return true
 end
