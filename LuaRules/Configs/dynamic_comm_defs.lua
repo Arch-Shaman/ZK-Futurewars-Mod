3decc3cecc258f90fdcd37cb931807ac9fbfa7dd
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-01-25 10:02:35 -0500

    Remove NYI
    Fixes #99

--- LuaRules/Configs/dynamic_comm_defs.lua
+++ LuaRules/Configs/dynamic_comm_defs.lua
@@ -436,7 +436,7 @@ local moduleDefs = {
 	{
 		name = "commweapon_disruptorprojector",
 		humanName = "Disruptor Projector (Area Slow)",
-		description = "Deals some damage and slows targets in a small area. Low DPS. Can be converted into a heavy AOE slow beam. (NYI)",
+		description = "Deals some damage and slows targets in a small area. Low DPS. Can be converted into a heavy AOE slow beam.",
 		image = moduleImagePath .. "commweapon_disruptorprojector.png",
 		limit = 1,
 		cost = 20 * COST_MULT,
