8cd73af0eaa56759565d5fb8f29e2bf733195f68
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-02-06 23:03:20 -0500

    Whitespace + changes
    Co-Authored-By: Owen <sharksinteractive@gmail.com>

--- units/singularity.lua
+++ units/singularity.lua
@@ -1,118 +1,102 @@
-return { singularity = {
-  unitname                      = [[singularity]],
-  name                          = [[Quake]],
-  description                   = [[Seismic Missile]],
-  buildCostMetal                = 400,
-  builder                       = false,
-  buildingGroundDecalDecaySpeed = 30,
-  buildingGroundDecalSizeX      = 3,
-  buildingGroundDecalSizeY      = 3,
-  buildingGroundDecalType       = [[seismic_aoplane.dds]],
-  buildPic                      = [[singularity.png]],
-  category                      = [[SINK UNARMED]],
-  collisionVolumeOffsets        = [[0 15 0]],
-  collisionVolumeScales         = [[20 50 20]],
-  collisionVolumeType           = [[CylY]],
+return { 
+	singularity = {
+		unitname                      = [[singularity]],
+		name                          = [[Wake]],
+		description                   = [[Singularity Missile]],
+		buildCostMetal                = 700,
+		builder                       = false,
+		buildingGroundDecalDecaySpeed = 30,
+		buildingGroundDecalSizeX      = 3,
+		buildingGroundDecalSizeY      = 3,
+		buildingGroundDecalType       = [[seismic_aoplane.dds]],
+		buildPic                      = [[singularity.png]],
+		category                      = [[SINK UNARMED]],
+		collisionVolumeOffsets        = [[0 15 0]],
+		collisionVolumeScales         = [[20 50 20]],
+		collisionVolumeType           = [[CylY]],
 
-  customParams                  = {
-    mobilebuilding = [[1]],
-  },
+		customParams                  = {
+			mobilebuilding = [[1]],
+		},
 
-  explodeAs                     = [[SEISMIC_WEAPON]],
-  footprintX                    = 1,
-  footprintZ                    = 1,
-  iconType                      = [[cruisemissilesmall]],
-  idleAutoHeal                  = 5,
-  idleTime                      = 1800,
-  maxDamage                     = 1000,
-  maxSlope                      = 18,
-  minCloakDistance              = 150,
-  objectName                    = [[missilesingu.dae]],
-  script                        = [[cruisemissile.lua]],
-  selfDestructAs                = [[SEISMIC_WEAPON]],
+		explodeAs                     = [[SEISMIC_WEAPON]],
+		footprintX                    = 1,
+		footprintZ                    = 1,
+		iconType                      = [[cruisemissilesmall]],
+		idleAutoHeal                  = 5,
+		idleTime                      = 1800,
+		maxDamage                     = 1000,
+		maxSlope                      = 18,
+		minCloakDistance              = 150,
+		objectName                    = [[missilesingu.dae]],
+		script                        = [[cruisemissile.lua]],
+		selfDestructAs                = [[SEISMIC_WEAPON]],
 
-  sfxtypes                      = {
+		sfxtypes                      = {
+			explosiongenerators = {
+				[[custom:RAIDMUZZLE]],
+			},
+		},
+		sightDistance                 = 0,
+		useBuildingGroundDecal        = false,
+		yardMap                       = [[o]],
+		weapons                       = {
+			{
+				def                = [[SEISMIC_WEAPON]],
+				badTargetCategory  = [[SWIM LAND SHIP HOVER]],
+				onlyTargetCategory = [[SWIM LAND SINK TURRET FLOAT SHIP HOVER]],
+			},
+		},
 
-    explosiongenerators = {
-      [[custom:RAIDMUZZLE]],
-    },
+		weaponDefs                    = {
 
-  },
-
-  sightDistance                 = 0,
-  useBuildingGroundDecal        = false,
-  yardMap                       = [[o]],
-
-  weapons                       = {
-
-    {
-      def                = [[SEISMIC_WEAPON]],
-      badTargetCategory  = [[SWIM LAND SHIP HOVER]],
-      onlyTargetCategory = [[SWIM LAND SINK TURRET FLOAT SHIP HOVER]],
-    },
-
-  },
-
-  weaponDefs                    = {
-
-    SEISMIC_WEAPON = {
-      name                    = [[Seismic Missile]],
-      areaOfEffect            = 640,
-      avoidFriendly           = false,
-      cegTag                  = [[seismictrail]],
-      collideFriendly         = false,
-      craterBoost             = 32,
-      craterMult              = 1,
-
-      customParams            = {
-        gatherradius = [[416]],
-        smoothradius = [[320]],
-        detachmentradius = [[320]],
-        smoothmult   = [[1]],
-        smoothexponent = [[0.45]],
-        movestructures = [[1]],
-		singularity = [[1]],
-		singuradius = [[300]],
-		singulifespan = [[300]],
-		singustrength = [[17]],
-        restrict_in_widgets = 1,
-
-        stats_hide_dps = 1, -- one use
-        stats_hide_reload = 1,
-        
-        light_color = [[1.2 1.6 0.55]],
-        light_radius = 550,
-      },
+			SEISMIC_WEAPON = {
+				name                    = [[Seismic Missile]],
+				areaOfEffect            = 640,
+				avoidFriendly           = false,
+				cegTag                  = [[seismictrail]],
+				collideFriendly         = false,
+				craterBoost             = 32,
+				craterMult              = 1,
+				customParams            = {
+					singularity = [[1]],
+					singuradius = [[300]],
+					singulifespan = [[540]],
+					singustrength = [[20]],
+					restrict_in_widgets = 1,
+					stats_hide_dps = 1, -- one use
+					stats_hide_reload = 1,
+					
+					light_color = [[1.2 1.6 0.55]],
+					light_radius = 550,
+				},
       
-      damage                  = {
-        default = 20,
-        subs    = 1,
-      },
+				damage                  = {
+					default = 10,
+				},
+				edgeEffectiveness       = 0.4,
+				explosionGenerator      = [[custom:bull_fade]],
+				fireStarter             = 0,
+				flightTime              = 100,
+				interceptedByShieldType = 1,
+				model                   = [[missilesingu.dae]],
+				noSelfDamage            = true,
+				range                   = 4200,
+				reloadtime              = 10,
+				smokeTrail              = false,
+				soundHit                = [[explosion/ex_large4]],
+				soundStart              = [[weapon/missile/tacnuke_launch]],
+				tolerance               = 4000,
+				turnrate                = 18000,
+				waterWeapon             = true,
+				weaponAcceleration      = 180,
+				weaponTimer             = 3,
+				weaponType              = [[StarburstLauncher]],
+				weaponVelocity          = 1200,
+			},
 
-      edgeEffectiveness       = 0.4,
-      explosionGenerator      = [[custom:bull_fade]],
-      fireStarter             = 0,
-      flightTime              = 100,
-      interceptedByShieldType = 1,
-      model                   = [[wep_seismic.s3o]],
-      noSelfDamage            = true,
-      range                   = 6000,
-      reloadtime              = 10,
-      smokeTrail              = false,
-      soundHit                = [[explosion/ex_large4]],
-      soundStart              = [[weapon/missile/tacnuke_launch]],
-      tolerance               = 4000,
-      turnrate                = 18000,
-      waterWeapon             = true,
-      weaponAcceleration      = 180,
-      weaponTimer             = 3,
-      weaponType              = [[StarburstLauncher]],
-      weaponVelocity          = 1200,
-    },
+		},
 
-  },
-
-  featureDefs                   = {
-  },
-
-} }
+		featureDefs                   = {},
+	} 
+}
