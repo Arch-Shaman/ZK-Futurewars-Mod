3edd19c9d790fde6f8b77ae1fe95343e3dd1caeb
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-02-07 11:26:21 -0500

    Add EMG to Recon

---
+++ gamedata/modularcomms/weapons/commweapon_emg.lua
@@ -0,0 +1,37 @@
+local name = "commweapon_emg"
+local weaponDef = {
+	name                    = [[Medium EMG Rifle]],
+	areaOfEffect            = 8,
+	craterBoost             = 0,
+	craterMult              = 0,
+
+	customParams            = {
+		is_unit_weapon = 1,
+		muzzleEffectFire = [[custom:HEAVY_CANNON_MUZZLE]],
+		miscEffectFire = [[custom:emg_shells_l]],
+
+		light_color = [[0.8 0.76 0.38]],
+		light_radius = 120,
+		reaim_time = 1,
+	},
+
+	damage                  = {
+		default = 25.1,
+	},
+
+	edgeEffectiveness       = 0.5,
+	explosionGenerator      = [[custom:EMG_HIT_HE]],
+	impulseBoost            = 0,
+	impulseFactor           = 0.4,
+	interceptedByShieldType = 1,
+	myGravity               = 0.1,
+	range                   = 300,
+	reloadtime              = .1,
+	soundHit                = [[weapon/cannon/cannon_hit1]],
+	soundStart              = [[weapon/heavy_machinegun]],
+	turret                  = true,
+	weaponType              = [[Cannon]],
+	weaponVelocity          = 580,
+}
+
+return name, weaponDef
