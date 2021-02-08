7a9ae90b7c84ca9a40d23b803463cd5e4a901466
HTMLPhoton <61311312+HTMLPhoton@users.noreply.github.com>
2021-02-08 19:43:11 +1100

    Some things

---
+++ units/spiderskirm.lua
@@ -0,0 +1,185 @@
+return { spiderskirm = {
+  unitname               = [[spiderskirm]],
+  name                   = [[Twilight]],
+  description            = [[Skirmisher Spider (Indirect Fire)]],
+  acceleration           = 0.78,
+  brakeRate              = 4.68,
+  buildCostMetal         = 280,
+  buildPic               = [[spiderskirm.png]],
+  canGuard               = true,
+  canMove                = true,
+  canPatrol              = true,
+  category               = [[LAND]],
+  corpse                 = [[DEAD]],
+
+  customParams           = {
+    midposoffset   = [[0 -5 0]],
+  },
+
+  explodeAs              = [[BIG_UNITEX]],
+  footprintX             = 3,
+  footprintZ             = 3,
+  iconType               = [[spiderskirm]],
+  idleAutoHeal           = 5,
+  idleTime               = 1800,
+  leaveTracks            = true,
+  maxDamage              = 650,
+  maxSlope               = 72,
+  maxVelocity            = 1.5,
+  maxWaterDepth          = 22,
+  minCloakDistance       = 75,
+  movementClass          = [[TKBOT3]],
+  noChaseCategory        = [[TERRAFORM FIXEDWING GUNSHIP SATELLITE SUB]],
+  objectName             = [[recluse.s3o]],
+  script                 = [[spiderskirm.lua]],
+  selfDestructAs         = [[BIG_UNITEX]],
+  sightDistance          = 627,
+  trackOffset            = 0,
+  trackStrength          = 10,
+  trackStretch           = 1,
+  trackType              = [[ChickenTrackPointyShort]],
+  trackWidth             = 52,
+  turnRate               = 1400,
+
+  weapons                = {
+
+    {
+      def                = [[ADV_ROCKET]],
+      badTargetCategory  = [[FIXEDWING GUNSHIP]],
+      onlyTargetCategory = [[LAND SINK TURRET SHIP SWIM FLOAT HOVER FIXEDWING GUNSHIP]],
+    },
+
+    {
+      def                = [[PARTICLEBEAM]],
+      onlyTargetCategory = [[NONE]],
+    },
+
+  },
+
+  weaponDefs             = {
+
+    ADV_ROCKET = {
+      name                    = [[ASD Rocket Volley]],
+      areaOfEffect            = 48,
+      burst                   = 3,
+      burstrate               = 0.4,
+      cegTag                  = [[missiletrailgreen]],
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      customParams        = {
+		numprojectiles1 = 14, -- how many of the weapondef we spawn. OPTIONAL. Default: 1.
+		projectile1 = "spiderskirm_particlebeam",
+		--spreadradius = 8, -- used in clusters. OPTIONAL. Default: 100.
+		clustervec1 = "randomxyz", -- accepted values: randomx, randomy, randomz, randomxy, randomxz, randomyz, random. OPTIONAL. default: random.
+		keepmomentum1 = 20,
+		use2ddist = 0, -- should we check 2d or 3d distance? OPTIONAL. Default: 0.
+		proxy = 1, -- check for nearby units?
+		useheight = 0,
+		timeoutspawn = 0,
+		spawndist = 150, -- at what distance should we spawn the projectile(s)? REQUIRED.
+		vradius1 = "0,0,0,0,0,0", -- velocity that is randomly added. covers range of +-vradius. OPTIONAL. Default: 4.2
+		groundimpact = 0,
+		spawnsfx1 = 2049,
+		
+        light_camera_height = 2500,
+        light_color = [[0.90 0.65 0.30]],
+        light_radius = 250,
+      },
+
+      damage                  = {
+        default = 134,
+      },
+
+      edgeEffectiveness       = 0.5,
+      fireStarter             = 70,
+      flightTime              = 4,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 2,
+      model                   = [[recluse_missile.s3o]],
+      noSelfDamage            = true,
+      predictBoost            = 0.75,
+      range                   = 570,
+      reloadtime              = 6,
+      smokeTrail              = true,
+      soundHit                = [[explosion/ex_small13]],
+      soundStart              = [[weapon/missile/missile_fire4]],
+      soundTrigger            = true,
+      startVelocity           = 150,
+      trajectoryHeight        = 1.5,
+      turnRate                = 4000,
+      turret                  = true,
+      weaponAcceleration      = 150,
+      weaponType              = [[MissileLauncher]],
+      weaponVelocity          = 400,
+      wobble                  = 9000,
+    },
+	
+	PARTICLEBEAM = {
+      name                    = [[Auto Particle Beam]],
+      beamDecay               = 0.85,
+      beamTime                = 0.1,
+      beamttl                 = 45,
+      canattackground         = false,
+      coreThickness           = 0.5,
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      customParams            = {
+        light_color = [[0.9 0.22 0.22]],
+        light_radius = 80,
+      },
+
+      damage                  = {
+        default = 69.42,
+      },
+
+
+      dynDamageExp            = 1,
+	  dynDamageRange          = 300,
+      dynDamageInverted       = false,
+      explosionGenerator      = [[custom:flash1red]],
+      fireStarter             = 100,
+      impactOnly              = true,
+      impulseFactor           = 0,
+      interceptedByShieldType = 1,
+      laserFlareSize          = 7.5,
+      minIntensity            = 1,
+      range                   = 225,
+      reloadtime              = 0.3,
+      rgbColor                = [[0.2 1 0.2]],
+      sprayAngle              = 6000,
+      soundStart              = [[weapon/laser/mini_laser]],
+      soundStartVolume        = 6,
+      thickness               = 5,
+      tolerance               = 8192,
+      turret                  = true,
+      weaponType              = [[BeamLaser]],
+    },
+
+  },
+
+  featureDefs            = {
+
+    DEAD  = {
+      blocking         = true,
+      featureDead      = [[HEAP]],
+      footprintX       = 3,
+      footprintZ       = 3,
+      collisionVolumeOffsets = [[0 0 0]],
+      collisionVolumeScales  = [[50 30 50]],
+      collisionVolumeType    = [[ellipsoid]],
+      object           = [[recluse_wreck.s3o]],
+    },
+
+    HEAP  = {
+      blocking         = false,
+      footprintX       = 3,
+      footprintZ       = 3,
+      object           = [[debris3x3a.s3o]],
+    },
+
+  },
+
+} }
---
+++ units/turretantiheavy.lua
@@ -0,0 +1,223 @@
+return { turretantiheavy = {
+  unitname                      = [[turretantiheavy]],
+  name                          = [[Azimuth]],
+  description                   = [[Tachyonic Anti-Heavy Turret - Requires connection to a 225 energy grid]],
+  activateWhenBuilt             = true,
+  buildCostMetal                = 2450,
+  builder                       = false,
+  buildingGroundDecalDecaySpeed = 30,
+  buildingGroundDecalSizeX      = 6,
+  buildingGroundDecalSizeY      = 6,
+  buildingGroundDecalType       = [[turretantiheavy_aoplane.dds]],
+  buildPic                      = [[turretantiheavy.png]],
+  category                      = [[SINK TURRET]],
+  collisionVolumeOffsets        = [[0 0 0]],
+  --collisionVolumeScales         = [[75 100 75]],
+  --collisionVolumeType           = [[CylY]],
+  corpse                        = [[DEAD]],
+
+  customParams                  = {
+
+    keeptooltip    = [[any string I want]],
+
+    neededlink     = 225,
+    pylonrange     = 50,
+
+    aimposoffset   = [[0 32 0]],
+    midposoffset   = [[0 0 0]],
+    modelradius    = [[40]],
+
+    dontfireatradarcommand = '0',
+  },
+
+  damageModifier                = 0.15,
+  explodeAs                     = [[ESTOR_BUILDING]],
+  footprintX                    = 4,
+  footprintZ                    = 4,
+  iconType                      = [[fixedtachyon]],
+  idleAutoHeal                  = 5,
+  idleTime                      = 1800,
+  losEmitHeight                 = 65,
+  maxDamage                     = 6000,
+  maxSlope                      = 18,
+  maxWaterDepth                 = 0,
+  minCloakDistance              = 150,
+  noChaseCategory               = [[FIXEDWING LAND SHIP SWIM GUNSHIP SUB HOVER]],
+  objectName                    = [[arm_annihilator.s3o]],
+  onoffable                     = true,
+  radarDistance                 = 2850,
+  radarEmitHeight               = 100,
+  script                        = [[turretantiheavy.lua]],
+  selfdestructas                = [[ESTOR_BUILDING]],
+  sfxtypes               = {
+    explosiongenerators = {
+      [[custom:ATAALASERFIRE]],
+    },
+  },
+  sightDistance                 = 780,
+  useBuildingGroundDecal        = true,
+  yardMap                       = [[oooo oooo oooo oooo]],
+
+  weapons                       = {
+
+    {
+      def                = [[ATA]],
+      badTargetCategory  = [[FIXEDWING GUNSHIP]],
+      onlyTargetCategory = [[SWIM LAND SHIP SINK TURRET FLOAT GUNSHIP FIXEDWING HOVER]],
+    },        
+	{
+      def                = [[BOGUS_PHASER]],
+      badTargetCategory  = [[]],
+      onlyTargetCategory = [[]],
+    },  
+
+
+  },
+
+  weaponDefs                    = {
+
+    ATA = { -- changing the name causes an Vanilla gadget to become borked
+      name                    = [[Tachyonic Feedback Loop]],
+      areaOfEffect            = 20,
+      avoidFeature            = false,
+      avoidNeutral            = false,
+      beamTime                = 1/30, --llt has a rof of 10/s. domi has 30/s. 
+      beamttl                 = 6,
+      coreThickness           = 0.5,
+      craterBoost             = 0,
+      craterMult              = 0,
+      
+      customParams            = {
+        light_color = [[1.6 1.05 2.25]],
+        light_radius = 320,
+		
+        stats_hide_damage = 1, -- continuous laser
+        stats_hide_reload = 1,
+		
+		--poweruse_overdrive_
+		
+		dmg_scaling = 1/30,
+		--dmg_scaling_overdrive_subtrahend = 225,
+		--dmg_scaling_overdrive_divisor = 420,
+		--dmg_scaling_overdrive_exponent = 0.5,
+		dmg_scaling_max = 10000,
+		--dmg_scaling_max_overdrive_subtrahend = 0,
+		--dmg_scaling_max_overdrive_divisor = 50,
+		--dmg_scaling_max_overdrive_exponent = 1,
+		dmg_scaling_keeptime = 4,
+		dmg_scaling_falloff = 10000,
+		
+		ceg_d_override = 2,
+		explosion_generator = [[ataalasergrow]],
+      },
+
+      damage                  = {
+        default = 40.1,
+        planes  = 40.1,
+        subs    = 40.1,
+      },
+
+      explosionGenerator      = [[custom:ataalaser]],
+      fireTolerance           = 8192, -- 45 degrees
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 0, --maybe set back to 1?
+      largeBeamLaser          = true,
+      laserFlareSize          = 16.94,
+      leadLimit               = 18,
+      minIntensity            = 1,
+      noSelfDamage            = true,
+      range                   = 1200,
+      reloadtime              = 1/30,
+      rgbColor                = [[1 0.25 0]],
+      soundStart              = [[weapon/laser/heavy_laser6]],
+      soundStartVolume        = 15,
+      texture1                = [[largelaser]],
+      texture2                = [[flare]],
+      texture3                = [[flare]],
+      texture4                = [[smallflare]],
+      thickness               = 16.94,
+      tolerance               = 10000,
+      turret                  = true,
+      weaponType              = [[BeamLaser]],
+      weaponVelocity          = 1400,
+    },
+	BOGUS_PHASER = {
+      name                    = [[!!BOGUS!!]],
+      areaOfEffect            = 20,
+      avoidFeature            = false,
+      avoidNeutral            = false,
+      beamTime                = 0.1,
+      canAttackGround         = false,
+	  collideEnemy            = false, --collides with shit, old code
+	  collideFriendly         = false,
+	  collideFeature          = false,
+	  collideNeutral          = false,
+	  collideGround           = false,
+	  commandFire             = true,
+      coreThickness           = 0.5,
+      craterBoost             = 0,
+      craterMult              = 0,
+      
+      customParams            = {
+
+        light_color = [[1.6 1.05 2.25]],
+        light_radius = 320,
+		bogus = 1,
+      },
+
+      damage                  = {
+        default = 420000,
+      },
+
+      explosionGenerator      = [[custom:riotballplus2_purple]],
+      fireTolerance           = 8192, -- 45 degrees
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 0,
+      largeBeamLaser          = true,
+      laserFlareSize          = 16.94,
+      leadLimit               = 18,
+      minIntensity            = 1,
+      noSelfDamage            = true,
+      range                   = 1200,
+      reloadtime              = 14,
+      rgbColor                = [[0.25 0 1]],
+      soundStart              = [[weapon/laser/heavy_laser6]],
+      soundStartVolume        = 15,
+      texture1                = [[largelaser]],
+      texture2                = [[flare]],
+      texture3                = [[flare]],
+      texture4                = [[smallflare]],
+      thickness               = 16.94,
+      tolerance               = 10000,
+      turret                  = true,
+      weaponType              = [[BeamLaser]],
+      weaponVelocity          = 1400,
+    },
+
+  },
+
+  featureDefs                   = {
+
+    DEAD = {
+      blocking         = true,
+      featureDead      = [[HEAP]],
+      footprintX       = 4,
+      footprintZ       = 4,
+      object           = [[arm_annihilator_dead.s3o]],
+    },
+
+
+    HEAP = {
+      blocking         = false,
+      footprintX       = 4,
+      footprintZ       = 4,
+      object           = [[debris3x3a.s3o]],
+    },
+
+  },
+
+} }
