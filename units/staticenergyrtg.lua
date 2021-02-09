ccde95f924f89180b35f4f6448c78d588aa45589
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-02-08 22:24:26 -0500

    opps

--- units/staticenergyrtg.lua
+++ units/staticenergyrtg.lua
@@ -22,7 +22,7 @@ return {
 			removestop     = 1,
 			priority_misc  = 2, -- High
 			decay_time     = 2,
-			decay_minoutput= 0.10,
+			decay_minoutput= 1.5,
 			decay_rate     = 0.025,
 			modelradius    = [[60]],
 			pylonrange = 220,
