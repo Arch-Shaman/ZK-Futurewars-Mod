10d0fc008eadb872e860cb4091c7ef091223d2d4
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-02-15 21:55:00 -0500

    Advanced Radar
    Cost: 500 -> 350 (300 from Radar)
    HP: 330 -> 1400 (4x hp:cost)
    Radar: 4000 -> 5000
    Radar Emit Height: 32 -> 150
    Sight: 800 -> 1700
    
    This is to make it marginally more useful as a spotter.

--- units/staticheavyradar.lua
+++ units/staticheavyradar.lua
@@ -2,9 +2,9 @@ return {
 	staticheavyradar = {
 		unitname                      = [[staticheavyradar]],
 		name                          = [[Advanced Radar]],
-		description                   = [[Long-Range Radar]],
+		description                   = [[Over-the-Horizon Radar]],
 		activateWhenBuilt             = true,
-		buildCostMetal                = 500,
+		buildCostMetal                = 350,
 		builder                       = false,
 		buildingGroundDecalDecaySpeed = 30,
 		buildingGroundDecalSizeX      = 4,
@@ -16,7 +16,6 @@ return {
 		collisionVolumeScales         = [[32 83 32]],
 		collisionVolumeType           = [[CylY]],
 		corpse                        = [[DEAD]],
-
 		customParams                  = {
 			modelradius    = [[16]],
 			removewait     = 1,
@@ -33,21 +32,19 @@ return {
 		idleAutoHeal                  = 5,
 		idleTime                      = 1800,
 		levelGround                   = false,
-		maxDamage                     = 330,
+		maxDamage                     = 1400,
 		maxSlope                      = 36,
 		minCloakDistance              = 150,
 		objectName                    = [[novaradar.s3o]],
 		script                        = [[staticheavyradar.lua]],
 		onoffable                     = true,
-		radarDistance                 = 4000,
-		radarEmitHeight               = 32,
+		radarDistance                 = 5000,
+		radarEmitHeight               = 150,
 		selfDestructAs                = [[SMALL_BUILDINGEX]],
-		sightDistance                 = 800,
+		sightDistance                 = 1700,
 		useBuildingGroundDecal        = true,
 		yardMap                       = [[oooo]],
-		
 		featureDefs                   = {
-			
 			DEAD  = {
 				blocking         = true,
 				featureDead      = [[HEAP]],
@@ -62,7 +59,6 @@ return {
 				footprintZ       = 2,
 				object           = [[debris2x2b.s3o]],
 			},
-
 		},
 	}
 }
