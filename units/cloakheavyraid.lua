ead55386a19528997770ae777755800dd19006c3
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-02-25 20:49:36 -0500

    Assassin
    Decloak range: 145 -> 100
    
    See how this plays out.

--- units/cloakheavyraid.lua
+++ units/cloakheavyraid.lua
@@ -39,7 +39,7 @@ return {
 		maxSlope               = 36,
 		maxVelocity            = 3.8,
 		maxWaterDepth          = 22,
-		minCloakDistance       = 145,
+		minCloakDistance       = 100,
 		movementClass          = [[KBOT2]],
 		moveState              = 0,
 		noChaseCategory        = [[TERRAFORM FIXEDWING SUB]],
