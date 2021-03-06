899df57a471cf553bd97d09c6e77558570ad9a10
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-03-06 07:39:36 -0500

    fix complaints about cluster grenade

--- gamedata/modularcomms/subprojectiles/commweapon_clustergrenade_fragment_dummy.lua
+++ gamedata/modularcomms/subprojectiles/commweapon_clustergrenade_fragment_dummy.lua
@@ -1,4 +1,4 @@
-local name = "commweapon_clustergrenade_fragment"
+local name = "commweapon_clustergrenade_fragment_dummy"
 weaponDef = {
 	name                    = [[Cluster Grenade Fragment]],
 	accuracy                = 400,
@@ -12,9 +12,24 @@ weaponDef = {
 		light_camera_height = 2500,
 		light_color = [[0.25 0.13 0.05]],
 		light_radius = 500,
+		numprojectiles1 = 1, -- how many of the weapondef we spawn. OPTIONAL. Default: 1.
+		projectile1 = "commweapon_napalmgrenade_fragment",
+		--spreadradius = 8, -- used in clusters. OPTIONAL. Default: 100.
+		clustervec1 = "derpderpderpderpderpderpderpderpderpderp", -- accepted values: randomx, randomy, randomz, randomxy, randomxz, randomyz, random. OPTIONAL. default: random.
+		keepmomentum1 = 1,
+		timeoutspawn = 0,
+		noairburst = "I belive I can fly...", -- if true, this projectile will skip all airburst checks
+		spawndist = 69420, -- at what distance should we spawn the projectile(s)? REQUIRED.
+		timeddeploy = 20,
+		commsubmunition = 1,
+		--lups_heat_fx = [[firewalker]],
+		light_camera_height = 2500,
+		light_color = [[0.25 0.13 0.05]],
+		light_radius = 500,
+		shield_damage = 120,
 	},
 	damage                  = {
-		default = 120,
+		default = 0,
 	},
 	model                   = [[diskball.s3o]],
 	explosionGenerator      = [[custom:TORPEDOHITHUGE]],
@@ -22,20 +37,20 @@ weaponDef = {
 	impulseBoost            = 0,
 	impulseFactor           = 0.4,
 	interceptedByShieldType = 1,
-	myGravity               = 0.25,
+	myGravity               = 0.1,
+	noExplode               = true,
 	projectiles             = 10,
 	range                   = 900,
 	reloadtime              = 12,
 	rgbColor                = [[1 0.5 0.2]],
 	size                    = 5,
-	soundHit                = [[weapon/clusters/napalm_break]],
+	--soundHit                = [[weapon/burn_mixed]],
 	soundStart              = [[weapon/cannon/wolverine_fire]],
 	soundStartVolume        = 3.2,
 	sprayangle              = 2500,
 	turret                  = true,
 	weaponType              = [[Cannon]],
 	weaponVelocity          = 320,
-	waterweapon				= true,
 }
 
 return name, weaponDef
