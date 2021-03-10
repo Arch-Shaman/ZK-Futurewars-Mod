7cffe91c3d0e1d3473f5fb976db1db99b5a56586
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-03-10 11:01:30 -0500

    Spread flak a bit more

--- units/tankaa.lua
+++ units/tankaa.lua
@@ -87,7 +87,7 @@ return {
 					clustervec1 = "randomxyz", -- accepted values: randomx, randomy, randomz, randomxy, randomxz, randomyz, random. OPTIONAL. default: random.
 					use2ddist = 0, -- should we check 2d or 3d distance? OPTIONAL. Default: 0.
 					spawndist = 80, -- at what distance should we spawn the projectile(s)? REQUIRED.
-					vradius1 = "-1,-1,-1,1,1,1", -- velocity that is randomly added. covers range of +-vradius. OPTIONAL. Default: 4.2
+					vradius1 = "-3,-1,-3,3,1,3", -- velocity that is randomly added. covers range of +-vradius. OPTIONAL. Default: 4.2
 					groundimpact = 1, -- check the distance between ground and projectile? OPTIONAL.
 					proxy = 1, -- check for nearby units?
 					damage_vs_shield = [[160]]
