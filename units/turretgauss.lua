0492d2bc1bb294a60e55b17cbc38c509691307cc
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-03-12 16:38:17 -0500

    Gauss
    HP: 3000 -> 2400

--- units/turretgauss.lua
+++ units/turretgauss.lua
@@ -34,7 +34,7 @@ return {
 		idleAutoHeal                  = 5,
 		idleTime                      = 1800,
 		levelGround                   = false,
-		maxDamage                     = 3000,
+		maxDamage                     = 2400,
 		maxSlope                      = 18,
 		maxVelocity                   = 0,
 		--maxWaterDepth                 = 0, -- model-derived would be 305: 35 elmo legs + 6x45 elmo pillar segments should be enough for everyone
