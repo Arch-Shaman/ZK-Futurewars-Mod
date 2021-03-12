2962507463ca0e7b4e8978c088a13978bb113ed4
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-03-12 13:46:39 -0500

    Increase starting metal back to 400

---
+++ LuaRules/Configs/start_setup.lua
@@ -0,0 +1,42 @@
+aiCommanders = {}
+for unitDefID, unitDef in pairs(UnitDefs) do
+	if unitDef.customParams.ai_start_unit then
+		aiCommanders[unitDefID] = true
+	end
+end
+
+ploppables = {
+  "factoryhover",
+  "factoryveh",
+  "factorytank",
+  "factoryshield",
+  "factorycloak",
+  "factoryamph",
+  "factoryjump",
+  "factoryspider",
+  "factoryship",
+  "factoryplane",
+  "factorygunship",
+}
+
+ploppableDefs = {}
+for i = 1, #ploppables do
+	local ud = UnitDefNames[ploppables[i]]
+	if ud and ud.id then
+		ploppableDefs[ud.id ] = true
+	end
+end
+
+-- starting resources
+START_METAL   = 400
+START_ENERGY  = 400
+
+INNATE_INC_METAL   = 2
+INNATE_INC_ENERGY  = 2
+
+START_STORAGE = 0
+
+COMM_SELECT_TIMEOUT = 30 * 15 -- 15 seconds
+
+DEFAULT_UNIT = UnitDefNames["dyntrainer_strike_base"].id
+DEFAULT_UNIT_NAME = "Strike Trainer"
