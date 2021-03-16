4b5fdbda3e621cde9a5c16f1d0592c1a5b80d4f2
HTMLPhoton <61311312+HTMLPhoton@users.noreply.github.com>
2021-03-16 17:59:01 +1100

    Add files via upload

--- LuaRules/Configs/setprojectiletargetdefs.lua
+++ LuaRules/Configs/setprojectiletargetdefs.lua
@@ -1,4 +1,8 @@
 local wantedefs = {}
-wantedefs[UnitDefNames["staticnuke"].id] = true
+wantedefs[UnitDefNames["staticnuke"].id] = {targets = 3, range = 1000}
+
+for index, data in pairs(wantedefs) do
+	data.range2 = data.range^2
+end
 
 return wantedefs
