40049ce3d806bf5fa33adc6eb3079f3559743655
HTMLPhoton <61311312+HTMLPhoton@users.noreply.github.com>
2021-03-18 23:59:23 +1100

    Add Commando
    Might be a bit dangerous, but let's see what happens.

--- units/spiderriot.lua
+++ units/spiderriot.lua
@@ -1,11 +1,12 @@
-return { 
+return {
 	spiderriot = {
 		unitname               = [[spiderriot]],
-		name                   = [[Redback]],
-		description            = [[Riot Spider]],
+		name                   = [[Commando]],
+		description            = [[Heavy Riot Spider]],
 		acceleration           = 0.66,
+		--autoheal               = 30,
 		brakeRate              = 3.96,
-		buildCostMetal         = 240,
+		buildCostMetal         = 460,
 		buildPic               = [[spiderriot.png]],
 		canGuard               = true,
 		canMove                = true,
@@ -15,20 +16,24 @@ return {
 		collisionVolumeScales  = [[36 36 36]],
 		collisionVolumeType    = [[ellipsoid]],
 		corpse                 = [[DEAD]],
-
+		
 		customParams           = {
-			aimposoffset       = [[0 10 0]],
-			aim_lookahead      = 80,
+			aimposoffset   = [[0 10 0]],
+			nanoregen = 10,
+			nano_maxregen = 3,
+			aim_lookahead = 80,
 		},
-
+		
 		explodeAs              = [[BIG_UNITEX]],
 		footprintX             = 3,
 		footprintZ             = 3,
+		highTrajectory         = 2,
 		iconType               = [[spiderriot]],
-		idleAutoHeal           = 5,
-		idleTime               = 1800,
+		idleAutoHeal           = 20,
+		idleTime               = 900,
 		leaveTracks            = true,
-		maxDamage              = 900,
+		losEmitHeight          = 40,
+		maxDamage              = 1100,
 		maxSlope               = 72,
 		maxVelocity            = 1.8,
 		maxWaterDepth          = 22,
@@ -38,70 +43,72 @@ return {
 		objectName             = [[spiderriot.s3o]],
 		script                 = [[spiderriot.lua]],
 		selfDestructAs         = [[BIG_UNITEX]],
-		sightDistance          = 366,
+		sightDistance          = 400,
 		trackOffset            = 0,
 		trackStrength          = 10,
 		trackStretch           = 1,
 		trackType              = [[ChickenTrackPointyShort]],
 		trackWidth             = 55,
 		turnRate               = 1700,
-
+		
 		weapons                = {
-
+		
 			{
-				def                = [[PARTICLEBEAM]],
-				badTargetCategory  = [[FIXEDWING]],
-				onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+			def                = [[NAPALM_SPRAYER]],
+			badTargetCategory  = [[FIXEDWING]],
+			onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
 			},
-
+		
 		},
-
+		
 		weaponDefs             = {
-
-			PARTICLEBEAM = {
-				name                    = [[Auto Particle Beam]],
-				beamDecay               = 0.85,
-				beamTime                = 1/30,
-				beamttl                 = 45,
-				coreThickness           = 0.5,
-				craterBoost             = 0,
-				craterMult              = 0,
-
-				customParams            = {
-					light_color = [[0.9 0.22 0.22]],
-					light_radius = 80,
+		
+			NAPALM_SPRAYER = {
+				name                    = [[Napalm Machine Gun]],
+				accuracy                = 500,
+				areaOfEffect            = 128,
+				avoidFeature            = false,
+				craterBoost             = 1,
+				craterMult              = 2,
+				cegTag                  = [[flamer]],
+			
+				customParams              = {
+					setunitsonfire = "1",
+					
 					sweepfire = 1,
 					sweepfire_maxangle = 25,
 					sweepfire_step = 3,
 					sweepfire_fastupdate = 1,
 				},
-
+				
 				damage                  = {
-				default = 70.01,
+					default = 20,
 				},
-
-				explosionGenerator      = [[custom:flash1red]],
-				fireStarter             = 100,
-				impactOnly              = true,
-				impulseFactor           = 0,
+			
+				explosionGenerator      = [[custom:napalm_phoenix]],
+				firestarter             = 180,
+				impulseBoost            = 0,
+				impulseFactor           = 0.4,
 				interceptedByShieldType = 1,
-				laserFlareSize          = 7.5,
-				minIntensity            = 1,
-				range                   = 300,
-				reloadtime              = 0.3,
-				rgbColor                = [[1 0 0]],
-				soundStart              = [[weapon/laser/mini_laser]],
-				soundStartVolume        = 6,
-				thickness               = 5,
-				tolerance               = 8192,
+				myGravity               = 0.45,
+				--projectiles             = 10,
+				range                   = 370,
+				reloadtime              = 0.067,
+				rgbColor                = [[1 0.5 0.2]],
+				size                    = 5,
+				soundHit                = [[weapon/cannon/wolverine_hit]],
+				soundStart              = [[weapon/cannon/wolverine_fire]],
+				soundStartVolume        = 3.2,
+				sprayangle              = 2500,
 				turret                  = true,
-				weaponType              = [[BeamLaser]],
+				weaponType              = [[Cannon]],
+				weaponVelocity          = 400,
 			},
-
+			
 		},
-
+		
 		featureDefs            = {
-
+		
 			DEAD  = {
 				blocking         = true,
 				featureDead      = [[HEAP]],
@@ -109,13 +116,14 @@ return {
 				footprintZ       = 3,
 				object           = [[tarantula_dead.s3o]],
 			},
-
+		
 			HEAP  = {
 				blocking         = false,
 				footprintX       = 3,
 				footprintZ       = 3,
 				object           = [[debris3x3a.s3o]],
 			},
+		
 		},
-	} 
-}
+
+} }
