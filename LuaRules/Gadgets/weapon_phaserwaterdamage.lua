fb64cd13255c35b8b460433adf599f6efadf25ac
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-03-21 00:46:07 -0400

    Add non-overdriven locust

---
+++ LuaRules/Gadgets/weapon_phaserwaterdamage.lua
@@ -0,0 +1,43 @@
+
+function gadget:GetInfo()
+  return {
+    name      = "Underwater Phaser Damage Reduction",
+    desc      = "Handle phaser damage underwater",
+    author    = "Shaman",
+    date      = "03/20/2021",
+    license   = "CC-0",
+    layer     = 0,
+    enabled   = true,
+  }
+end
+
+local config = {}
+
+for i = 1, #WeaponDefs do
+	local wd = WeaponDefs[i]
+	if wd.customParams.underwaterdamagereduction then
+		config[i] = tonumber(wd.customParams.underwaterdamagereduction) or 1
+		--Script.SetWatchWeapon(i, true)
+	end
+end
+
+local spGetUnitPosition = Spring.GetUnitPosition
+local sqrt = math.sqrt
+local min = math.min
+local abs = math.abs
+
+function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
+	if config[weaponDefID] then
+		local _, depth = spGetUnitPosition(unitID)
+		if depth >= -10 then
+			return damage, 1.0
+		else
+			local mult = config[weaponDefID]
+			depth = abs(depth)
+			local depthmod = min(1/(sqrt(depth + 10) * mult), 1)
+			return damage * depthmod, 1.0
+		end
+	else
+		return damage, 1.0
+	end
+end
