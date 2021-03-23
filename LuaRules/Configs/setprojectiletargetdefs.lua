978ae32a2d35a0424ae3258c95f2f3e8cb398078
HTMLPhoton <61311312+HTMLPhoton@users.noreply.github.com>
2021-03-23 23:25:07 +1100

    Update setprojectiletargetdefs.lua

--- LuaRules/Configs/setprojectiletargetdefs.lua
+++ LuaRules/Configs/setprojectiletargetdefs.lua
@@ -1,5 +1,6 @@
 local wantedefs = {}
-wantedefs[UnitDefNames["staticnuke"].id] = {targets = 3, range = 1750}
+wantedefs[UnitDefNames["staticnuke"].id] = {targets = 3, range = 1750, UIaoe = 800}
+wantedefs[UnitDefNames["subtacmissile"].id] = {targets = 6, range = 500, UIaoe = 80}
 
 for index, data in pairs(wantedefs) do
 	data.range2 = data.range^2
