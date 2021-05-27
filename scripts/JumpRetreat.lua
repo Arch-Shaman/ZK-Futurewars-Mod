c56dc4d63e16d11c7e14576929a56a310d229bff
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-05-26 21:41:08 -0400

    Jump retreat no longer jumps at low distances

--- scripts/JumpRetreat.lua
+++ scripts/JumpRetreat.lua
@@ -2,6 +2,7 @@
 local CMD_JUMP = Spring.Utilities.CMD.JUMP
 local GiveClampedOrderToUnit = Spring.Utilities.GiveClampedOrderToUnit
 local jumpRange = tonumber(UnitDefs[unitDefID].customParams.jump_range)
+local retreattype = UnitDefs[unitDefID].customParams.jumpretreattype or "always"
 
 local retreating = false
 
@@ -13,12 +14,15 @@ local function RetreatThread(hx, hy, hz)
 		if reload >= 1 and not disarmed then
 			local ux, uy, uz = Spring.GetUnitPosition(unitID)
 			local moveDistance = math.sqrt(((ux - hx) * (ux - hx)) + ((uz - hz) * (uz - hz)))
-			if moveDistance < jumpRange then
-				Sleep(1000)
+			if moveDistance < jumpRange / 2 and moveDistance < 300 then -- don't jump around in haven or waste it near it.
+				retreating = false
 			else
 				local disScale = jumpRange/moveDistance*0.95
 				local cx, cy, cz = ux + disScale*(hx - ux), hy, uz + disScale*(hz - uz)
 				GiveClampedOrderToUnit(unitID, CMD.INSERT, { 0, CMD_JUMP, CMD.OPT_INTERNAL, cx, cy, cz}, CMD.OPT_ALT)
+				if retreattype == "once" then
+					retreating = false
+				end
 			end
 		else
 			Sleep(33)
@@ -28,6 +32,12 @@ end
 		
 
 function RetreatFunction(hx, hy, hz)
+	if retreattype == "none" then
+		return
+	end
+	if Spring.GetUnitRulesParam(unitID, "comm_jumprange_bonus") then -- dynacomm support (FW ONLY)
+		jumpRange = Spring.GetUnitRulesParam(unitID, "comm_jumprange_bonus")
+	end
 	retreating = true
 	StartThread(RetreatThread, hx, hy, hz)
 end
