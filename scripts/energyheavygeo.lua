c99015c78953e90ce91cd72a6746c6981fa520c3
HTMLPhoton <61311312+HTMLPhoton@users.noreply.github.com>
2021-06-29 18:11:16 +1000

    Moho Geo LUS

---
+++ scripts/energyheavygeo.lua
@@ -0,0 +1,51 @@
+include "constants.lua"
+
+local base = piece "base"
+local smoke1 = piece "smoke1"
+local smoke2 = piece "smoke2"
+local smoke3 = piece "smoke3"
+
+function script.Create()
+	Spin (smoke1, y_axis, math.rad(1000))
+	StartThread(GG.Script.SmokeUnit, unitID, {smoke1, smoke2, smoke2, smoke3, smoke3, smoke3}, 6)
+end
+
+function script.AimFromWeapon()
+	return base
+end
+
+function script.QueryWeapon()
+	return base
+end
+
+function script.AimWeapon(num, heading, pitch)
+	return false
+end
+
+function script.BlockShot(num)
+	return true
+end
+
+function script.Killed(recentDamage, maxHealth)
+	local px, py, pz = Spring.GetUnitPosition(unitID)
+	Spring.SpawnProjectile(WeaponDefNames["energyheavygeo_yellowstone"].id, {
+		pos = {px, py, pz},
+		["end"] = {px, py, pz},
+		speed = {0, 0, 0},
+		ttl = 10,
+		gravity = 0,
+		team = Spring.GetGaiaTeamID(),
+		owner = unitID,
+	})
+	local severity = recentDamage / maxHealth
+	if (severity <= .25) then
+		Explode(base, SFX.NONE)
+		return 1 -- corpsetype
+	elseif (severity <= .5) then
+		Explode(base, SFX.NONE)
+		return 1 -- corpsetype
+	else
+		Explode(base, SFX.SHATTER)
+		return 2 -- corpsetype
+	end
+end
