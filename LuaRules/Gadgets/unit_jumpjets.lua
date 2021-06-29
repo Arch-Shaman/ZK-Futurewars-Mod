fda502cd8a94c904bb2cd596db2ab975da4da03e
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-06-29 05:33:39 -0400

    Update jumpjets from stable

--- LuaRules/Gadgets/unit_jumpjets.lua
+++ LuaRules/Gadgets/unit_jumpjets.lua
@@ -100,6 +100,13 @@ for udid = 1, #UnitDefs do
 	end
 end
 
+local spUnitScript = Spring.UnitScript -- CallAsUnit can't be localized directly because a later-layered gadget modifies it
+local function CallAsUnitIfExists(unitID, func, ...)
+	if func then
+		spUnitScript.CallAsUnit(unitID, func, ...)
+	end
+end
+
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
@@ -217,6 +224,7 @@ local function Jump(unitID, goal, origCmdParams, mustJump)
 	start[2] = math.max(start[2], startHeight)
 	
 	local reloadmult	   = math.max(1 - (spGetUnitRulesParam(unitID, "comm_jumpreload_bonus") or 0), 0)
+	local fakeUnitID
 	local unitDefID        = spGetUnitDefID(unitID)
 	local jumpDef          = jumpDefs[unitDefID]
 	local defSpeed         = jumpDef.speed + speedbonus
@@ -231,7 +239,6 @@ local function Jump(unitID, goal, origCmdParams, mustJump)
 	end
 	
 	local rotateMidAir = jumpDef.rotateMidAir
-	local env
 	
 	local vector = {goal[1] - start[1],
 	                goal[2] - start[2],
@@ -290,10 +297,10 @@ local function Jump(unitID, goal, origCmdParams, mustJump)
 	Spring.SetUnitRulesParam(unitID, "jump_goal_z", goal[3], privateTable)
 	
 
-	env = Spring.UnitScript.GetScriptEnv(unitID)
+	local env = Spring.UnitScript.GetScriptEnv(unitID) or emptyTable
 	
 	if (delay == 0) then
-		Spring.UnitScript.CallAsUnit(unitID,env.beginJump,turn,lineDist,flightDist,duration)
+		CallAsUnitIfExists(unitID,env.beginJump,turn,lineDist,flightDist,duration)
 		if rotateMidAir then
 			mcSetRotation(unitID, 0, (2^15 - startHeading)/rotUnit, 0) -- keep current heading
 			mcSetRotationVelocity(unitID, 0, -turn/rotUnit*step, 0)
@@ -302,7 +309,7 @@ local function Jump(unitID, goal, origCmdParams, mustJump)
 			GG.PlayFogHiddenSound("Jump", UnitDefs[unitDefID].mass/10, start[1], start[2], start[3])
 		end
 	else
-		Spring.UnitScript.CallAsUnit(unitID,env.preJump,turn,lineDist,flightDist,duration)
+		CallAsUnitIfExists(unitID,env.preJump,turn,lineDist,flightDist,duration)
 	end
 	spSetUnitRulesParam(unitID,"jumpReload",0)
 
@@ -315,7 +322,7 @@ local function Jump(unitID, goal, origCmdParams, mustJump)
 			if (not Spring.ValidUnitID(unitID) or Spring.GetUnitIsDead(unitID)) then
 				return
 			end
-			Spring.UnitScript.CallAsUnit(unitID,env.beginJump)
+			CallAsUnitIfExists(unitID,env.beginJump)
 			if PLAY_SOUND and (not cannotJumpMidair) then	-- don't make sound if we jump with legs instead of jets
 				GG.PlayFogHiddenSound("Jump", UnitDefs[unitDefID].mass/10, start[1], start[2], start[3])
 			end
@@ -330,9 +337,7 @@ local function Jump(unitID, goal, origCmdParams, mustJump)
 		local attachedTransport = Spring.GetUnitTransporter(unitID)
 		if (attachedTransport) then
 			local envTrans = Spring.UnitScript.GetScriptEnv(attachedTransport)
-			if (envTrans.ForceDropUnit) then
-				Spring.UnitScript.CallAsUnit(attachedTransport,envTrans.ForceDropUnit)
-			end
+			CallAsUnitIfExists(attachedTransport, envTrans.ForceDropUnit)
 		end
 	
 		local hitStructure, structureCollisionData
@@ -355,10 +360,10 @@ local function Jump(unitID, goal, origCmdParams, mustJump)
 			Spring.MoveCtrl.SetVelocity(unitID, (x - lastX), (y - lastY), (z - lastZ))
 			lastX, lastY, lastZ = x, y, z
 
-			Spring.UnitScript.CallAsUnit(unitID, env.jumping, i * 100)
+			CallAsUnitIfExists(unitID, env.jumping, i * 100)
 		
 			if (not halfJump and i > 0.5) then
-				Spring.UnitScript.CallAsUnit(unitID,env.halfJump)
+				CallAsUnitIfExists(unitID,env.halfJump)
 				halfJump = true
 				
 				-- Do structure collision here to prevent early collision (perhaps a rejump onto the same structure?)
@@ -380,7 +385,7 @@ local function Jump(unitID, goal, origCmdParams, mustJump)
 			i = i + step
 		end
 
-		Spring.UnitScript.CallAsUnit(unitID,env.endJump)
+		CallAsUnitIfExists(unitID,env.endJump)
 		if PLAY_SOUND then
 			GG.PlayFogHiddenSound("JumpLand", UnitDefs[unitDefID].mass/10, goal[1], goal[2], goal[3])
 		end
