c6ce3282a9a8fa79d98f84e90a4fa7b5cfcb9d30
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-08-07 18:35:25 -0400

    Add baseline lightning gun

---
+++ gamedata/modularcomms/weapons/commweapon_lightninggun_improved.lua
@@ -0,0 +1,11 @@
+local _, def = VFS.Include("gamedata/modularcomms/weapons/commweapon_lightninggun.lua")
+
+def.name = "Heavy " .. def.name
+def.paralyzeTime = def.paralyzeTime + 2
+def.customParams.extra_damage_mult = def.customParams.extra_damage_mult * 1.25
+
+def.customParams.light_radius = def.customParams.light_radius + 20
+def.thickness = def.thickness + 3
+def.rgbColor = "0.65 0.65 1"
+
+return "commweapon_lightninggun_improved", def
