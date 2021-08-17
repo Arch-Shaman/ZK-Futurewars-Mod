b1ff29602fb0060a2893a1498d8655a059ed3b3e
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2021-08-17 06:52:37 -0400

    Silencer MkIII
    Cost: 3.2k -> 4.5k

--- units/missilenuke.lua
+++ units/missilenuke.lua
@@ -3,7 +3,7 @@ return {
 		unitname                      = [[missilenuke]],
 		name                          = [[Silencer Mk III]],
 		description                   = [[Nuclear Cruise Missile]],
-		buildCostMetal                = 3200,
+		buildCostMetal                = 4500,
 		builder                       = false,
 		buildingGroundDecalDecaySpeed = 30,
 		buildingGroundDecalSizeX      = 3,
