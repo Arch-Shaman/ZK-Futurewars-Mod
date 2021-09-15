b7469b0309662a3633fdea668ac78513b3e63178
fenrave <40793346+fenrave@users.noreply.github.com>
2021-09-15 06:44:41 -0500

    Missed a comma
    This could result in bad behavior from the lua interpreter.

--- LuaUI/Configs/border_menu_commands.lua
+++ LuaUI/Configs/border_menu_commands.lua
@@ -221,7 +221,7 @@ local overrides = {
 	[CMD_AREA_TERRA_MEX] = {caption = '', texture = imageDir .. 'Bold/mex.png'},
 
 	[CMD_JUMP] = {texture = imageDir .. 'Bold/jump.png'},
-	[CMD_SWEEPFIRE] = {texture = imageDir .. 'sweepfire.png'}
+	[CMD_SWEEPFIRE] = {texture = imageDir .. 'sweepfire.png'},
 	[CMD_SWEEPFIRE_CANCEL] = {texture = imageDir .. 'sweepfire_cancel.png'},
 	[CMD_FIND_PAD] = {caption = '', texture = imageDir .. 'Bold/rearm.png'},
 
