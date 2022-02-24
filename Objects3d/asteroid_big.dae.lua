8865f25d5413a6b9788d606a71f97731ec5d88ba
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-02-24 07:35:52 -0500

    Add a big ass meteor strike

---
+++ Objects3d/asteroid_big.dae.lua
@@ -0,0 +1,6 @@
+return {
+	radius = 30.0,
+	height = 30.0,
+	tex2 = "tatex/wreck007c00.BMP",
+	tex1 = "tatex/wreck001c00.BMP",
+}
---
+++ effects/meteortrailbig.lua
@@ -0,0 +1,25 @@
+return {
+	["meteorbig_tag"] = {
+		usedefaultexplosions = false,
+		fire = {
+			air                = true,
+			class              = [[CBitmapMuzzleFlame]],
+			count              = 1,
+			ground             = true,
+			underwater         = 1,
+			water              = true,
+			properties = {
+				alwaysvisible      = true,
+				colormap           = [[1 1 1 0.01	1 0.5 0 0.01	0 0 0 0.01]],
+				dir                = [[dir]],
+				frontoffset        = 100,
+				fronttexture       = [[plasma0029]],
+				length             = -1180,
+				sidetexture        = [[plasma2]],
+				size               = 400,
+				sizegrowth         = 30,
+				ttl                = 15,
+			},
+		},
+	},
+}
