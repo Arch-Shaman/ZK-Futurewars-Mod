344789fceae6eaa3054d8ab048f2fe3c0fedf062
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-02-25 04:24:59 -0500

    Frozen Planet
    Metal 2.0 -> 2.2
    Added a 4.0 supermex

---
+++ LuaRules/Configs/MetalSpots/FrozenPlanetV3.lua
@@ -0,0 +1,30 @@
+local normal = 2.2
+local super = 4.0
+
+return {
+	spots = {
+		{x = 104, z = 1224, metal = normal},
+		{x = 2424, z = 312, metal = normal},
+		{x = 3736, z = 2904, metal = normal},
+		{x = 4024, z = 1032, metal = normal},
+		{x = 3784, z = 888, metal = normal},
+		{x = 3928, z = 3144, metal = normal},
+		{x = 776, z = 1944, metal = normal},
+		{x = 2840, z = 1304, metal = normal},
+		{x = 1464, z = 1736, metal = normal},
+		{x = 1224, z = 2776, metal = normal},
+		{x = 1288, z = 216, metal = normal},
+		{x = 2952, z = 3896, metal = normal},
+		{x = 3960, z = 2792, metal = normal},
+		{x = 1704, z = 3784, metal = normal},
+		{x = 136, z = 3400, metal = normal},
+		{x = 440, z = 3240, metal = normal},
+		{x = 232, z = 3048, metal = normal},
+		{x = 136, z = 920, metal = normal},
+		{x = 3944, z = 712, metal = normal},
+		{x = 2568, z = 2504, metal = normal},
+		{x = 3272, z = 2136, metal = normal},
+		{x = 312, z = 1112, metal = normal},
+		{x = 2048, z = 2048, metal = super},
+	}
+}
