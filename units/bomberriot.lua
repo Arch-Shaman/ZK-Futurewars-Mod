78e352618343f3e047f2e1cabfc60cbc966f1bb8
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-02-28 08:44:48 -0500

    Light_radios -> Light_radius

--- units/bomberriot.lua
+++ units/bomberriot.lua
@@ -87,7 +87,7 @@ local unitDef = {
 				area_damage_duration = 16,
 				light_camera_height = 2500,
 				light_color = [[0.25 0.13 0.05]],
-				light_radios = 460,
+				light_radius = 460,
 				lups_napalm_fx = 1,
 				 
 			},
