978295d8bb37f5e38cf2c457b350450b93267f92
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-04-02 15:58:03 -0400

    Future Wared Firebreak

---
+++ LuaRules/Configs/MetalSpots/Firebreak v1.1.lua
@@ -0,0 +1,39 @@
+local normal = 3.0
+local super = 5.0
+
+return {
+	spots = {
+		{x = 1256, z = 3144, metal = normal},
+		{x = 5912, z = 1976, metal = normal},
+		{x = 1496, z = 3448, metal = normal},
+		{x = 5672, z = 1672, metal = normal},
+		{x = 1320, z = 3384, metal = normal},
+		{x = 5848, z = 1736, metal = normal},
+		{x = 1864, z = 3896, metal = normal},
+		{x = 5304, z = 1224, metal = normal},
+		{x = 1432, z = 2504, metal = normal},
+		{x = 5736, z = 2616, metal = normal},
+		{x = 1976, z = 1608, metal = normal},
+		{x = 5192, z = 3528, metal = normal},
+		{x = 3128, z = 920, metal = normal},
+		{x = 4040, z = 4200, metal = normal},
+		{x = 2776, z = 1000, metal = normal},
+		{x = 4392, z = 4120, metal = normal},
+		{x = 2408, z = 40, metal = normal},
+		{x = 4760, z = 5080, metal = normal},
+		{x = 2184, z = 264, metal = normal},
+		{x = 4984, z = 4856, metal = normal},
+		{x = 1208, z = 200, metal = super},
+		{x = 5976, z = 4920, metal = super},
+		{x = 1800, z = 4856, metal = normal},
+		{x = 5368, z = 264, metal = normal},
+		{x = 3048, z = 4776, metal = normal},
+		{x = 4120, z = 344, metal = normal},
+		{x = 2616, z = 3624, metal = normal},
+		{x = 4552, z = 1496, metal = normal},
+		{x = 3336, z = 3704, metal = normal},
+		{x = 3832, z = 1416, metal = normal},
+		{x = 2552, z = 2168, metal = normal},
+		{x = 4616, z = 2968, metal = normal},
+	}
+}
