2bcde82345f0e262032062664592a285969ab4b7
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-06-28 22:43:17 -0400

    Altair Crossing
    Mexes: 1.0 / 2.2 / 2.6 -> 2.0 / 3.0 / 4.0

---
+++ LuaRules/Configs/MetalSpots/Altair_Crossing_v3.lua
@@ -0,0 +1,38 @@
+local low = 2.0
+local normal = 3.0
+local super = 4.0
+
+return {
+	spots = {
+		{x = 3864, z = 2136, metal = normal},
+		{x =  888, z = 4024, metal = low},
+		{x = 3944, z =  184, metal = low},
+		{x =  568, z = 1928, metal = normal},
+		{x = 1528, z = 1560, metal = super},
+		{x = 3640, z = 2056, metal = normal},
+		{x =  568, z = 3144, metal = normal},
+		{x = 1912, z = 4040, metal = super},
+		{x = 1592, z = 4024, metal = low},
+		{x =  360, z = 1800, metal = normal},
+		{x = 3240, z = 3992, metal = low},
+		{x =  120, z = 3992, metal = low},
+		{x = 3720, z =  904, metal = normal},
+		{x =  552, z =  872, metal = normal},
+		{x = 2360, z = 4024, metal = low},
+		{x = 1784, z =  184, metal = low},
+		{x = 2392, z =  152, metal = low},
+		{x = 3736, z = 3096, metal = normal},
+		{x =  360, z = 3016, metal = normal},
+		{x = 1992, z =  120, metal = super},
+		{x =   88, z =  152, metal = low},
+		{x = 2392, z = 2376, metal = super},
+		{x =  360, z = 2024, metal = normal},
+		{x =  936, z =  280, metal = low},
+		{x = 3960, z = 3992, metal = low},
+		{x =  376, z =  968, metal = normal},
+		{x = 3096, z =  152, metal = low},
+		{x = 3544, z = 3288, metal = normal},
+		{x = 3800, z = 1848, metal = normal},
+		{x = 3528, z =  776, metal = normal},
+	}
+}
