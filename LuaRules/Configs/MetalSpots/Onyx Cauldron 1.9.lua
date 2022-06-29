b17696778cff6aadf7c273759fcce3d772e37424
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-06-28 20:29:07 -0400

    Onyx Cauldron
    Added 4 supermexes worth 7m/s
    All other mexes now worth 3m/s

---
+++ LuaRules/Configs/MetalSpots/Onyx Cauldron 1.9.lua
@@ -0,0 +1,39 @@
+local normal = 3
+local super  = 7
+
+return {
+	spots = {
+		{x = 2296, z = 2536, metal = normal},
+		{x = 3880, z = 7192, metal = normal},
+		{x = 6056, z = 3368, metal = normal},
+		{x = 5512, z = 2104, metal = normal},
+		{x = 2920, z = 4696, metal = normal},
+		{x = 7464, z = 3112, metal = normal},
+		{x = 1832, z = 4840, metal = normal},
+		{x = 5736, z = 7112, metal = normal},
+		{x = 2648, z = 2184, metal = normal},
+		{x = 2616, z = 7032, metal = normal},
+		{x = 7080, z = 6856, metal = normal},
+		{x = 6696, z = 6824, metal = normal},
+		{x = 2728, z = 6232, metal = normal},
+		{x = 7192, z = 2008, metal = super},
+		{x = 5480, z = 5400, metal = super},
+		{x = 3640, z = 5448, metal = normal},
+		{x = 5848, z = 6728, metal = normal},
+		{x = 4152, z = 2216, metal = normal},
+		{x = 4296, z = 6328, metal = normal},
+		{x = 2184, z = 6552, metal = normal},
+		{x = 7608, z = 2280, metal = super},
+		{x = 7192, z = 5464, metal = normal},
+		{x = 6920, z = 5272, metal = normal},
+		{x = 2376, z = 3240, metal = normal},
+		{x = 3944, z = 2024, metal = normal},
+		{x = 3064, z = 5960, metal = normal},
+		{x = 6408, z = 1816, metal = normal},
+		{x = 1992, z = 3352, metal = normal},
+		{x = 4008, z = 3656, metal = super},
+		{x = 6712, z = 7160, metal = normal},
+		{x = 2280, z = 2152, metal = normal},
+		{x = 7176, z = 4248, metal = normal},
+	}
+}
