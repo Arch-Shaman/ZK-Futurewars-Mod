9aa18d68d962f480a04cf059e18cc6c82b4aa899
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-07-02 01:17:49 -0400

    Potential fix to cobbler splitting

--- units/tankriot.lua
+++ units/tankriot.lua
@@ -105,7 +105,7 @@ return {
 					--spreadradius1 = 4, -- used in clusters. OPTIONAL. Default: 100.
 					clustervec1 = "randomxyz", -- accepted values: randomx, randomy, randomz, randomxy, randomxz, randomyz, random. OPTIONAL. default: random.
 					use2ddist = 0, -- should we check 2d or 3d distance? OPTIONAL. Default: 0.
-					spawndist = 110, -- at what distance should we spawn the projectile(s)? REQUIRED.
+					spawndist = 230, -- at what distance should we spawn the projectile(s)? REQUIRED.
 					timeoutspawn = 1, -- Can this missile spawn its subprojectiles when it times out? OPTIONAL. Default: 1.
 					vradius1 = "-1,-2,-1,1,-1,1", -- velocity that is randomly added. covers range of +-vradius. OPTIONAL. Default: 4.2
 					groundimpact = 1, -- check the distance between ground and projectile? OPTIONAL.
