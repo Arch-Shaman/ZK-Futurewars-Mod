db27f6ed006e4a2a807a7747ae485fe0984f4dfe
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-07-21 18:45:04 -0400

    Poorly Implement #410
    This is pretty funny though

---
+++ LuaRules/Gadgets/share_control.lua
@@ -0,0 +1,38 @@
+if (not gadgetHandler:IsSyncedCode()) then return end
+
+function gadget:GetInfo() return {
+	name      = "ShareControl",
+	desc      = "Controls sharing of units and resources",
+	author    = "trepan (Dave Rodgers)",
+	date      = "Apr 22, 2007",
+	license   = "GNU GPL, v2 or later",
+	layer     = -5,
+	enabled   = true,
+} end
+
+local spAreTeamsAllied = Spring.AreTeamsAllied
+local spIsCheatingEnabled = Spring.IsCheatingEnabled
+local spGetUnitRulesParam = Spring.GetUnitRulesParam
+local gaiaTeamID = Spring.GetGaiaTeamID()
+
+function gadget:AllowResourceTransfer(oldTeam, newTeam, resource_type, amount)
+	if ((amount < 0) or (not spAreTeamsAllied(oldTeam, newTeam))) then
+		return false
+	end
+	if resource_type == "m" and oldTeam ~= newTeam then -- Subsidization Strategy gained.
+		GG.Awards.AddAwardPoints('assistant', oldTeam, amount)
+	end
+	return true
+end
+
+function gadget:AllowUnitTransfer(unitID, unitDefID, oldTeam, newTeam, capture)
+	if (capture
+	or spAreTeamsAllied(oldTeam, newTeam)
+	or spIsCheatingEnabled()
+	or ((newTeam == gaiaTeamID) and (spGetUnitRulesParam(unitID, "can_share_to_gaia") == 1)) -- for Planet Wars
+	) then
+		return true
+	end
+
+	return false
+end
