148bba57f80458fdf3d00516a3d248f8ea98af82
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-09-14 01:54:19 -0400

    Improvements to scenario dumper
    * Now gets the unit building
    * Now records factory queues properly

--- LuaUI/Widgets/dev_scenariodumper.lua
+++ LuaUI/Widgets/dev_scenariodumper.lua
@@ -62,23 +62,60 @@ function widget:KeyPress(v)
 		Spring.Echo("Dumping units.")
 		local allUnits = Spring.GetAllUnits()
 		local file = io.open("luaui\\dumps\\scenarios\\" .. Game.mapName .. ".lua","w")
+		local builders = {}
+		local finaltable = {}
 		for i = 1, #allUnits do
 			local unitID = allUnits[i]
 			local x, y, z = Spring.GetUnitPosition(unitID)
 			local rx, ry, rz = Spring.GetUnitRotation(unitID)
 			local heading = GetUnitFacing(unitID)
 			local hp, maxhp, empdmg, captureprogress, buildprogress = Spring.GetUnitHealth(unitID)
+			local building = Spring.GetUnitIsBuilding(unitID) 
 			local unitTable = {
 				unitDef = UnitDefs[Spring.GetUnitDefID(unitID)].name,
 				positionState = {position = {x, y, z}, facing = heading, rotation = {rx, ry, rz}},
 				healthState = {hp = hp, maxhp = maxhp, paralysis = empdmg, captureprogress = captureprogress, buildprogress = buildprogress},
 				role = teamToRoleID[Spring.GetUnitTeam(unitID)],
 				allyID = Spring.GetUnitAllyTeam(unitID),
-				queue = Spring.GetCommandQueue(unitID, 99),
+				queue = {},
 				states = Spring.GetUnitStates(unitID),
+				builder = builders[i],
 			}
-			dumpstring = dumpstring .. "\n\t[" .. i .. "] = " .. TableToString(unitTable, 2)
+			local factoryqueue = Spring.GetFactoryCommands(unitID, 99)
+			local queue = Spring.GetCommandQueue(unitID, 99)
+			if factoryqueue then
+				for i = 1, #factoryqueue do
+					local cmd = factoryqueue[i]
+					unitTable.queue[#unitTable.queue + 1] = cmd
+				end
+			end
+			if queue and #queue > 0 then
+				for i = 1, #queue do
+					unitTable.queue[#unitTable.queue + 1] = queue[i]
+				end
+			end
+			if building then
+				for j = 1, #allUnits do
+					if allUnits[j] == building then
+						building = j
+						unitTable.building = building
+						if finaltable[j] then
+							finaltable[j].builder = i
+						else
+							builders[j] = i
+						end
+						if j < i then
+							table.insert(finaltable, j, unitTable)
+							break
+						end
+					end
+				end
+				builders[building] = i
+			else
+				finaltable[#finaltable+1] = unitTable
+			end
 		end
+		dumpstring = dumpstring .. TableToString(finaltable, 1)
 		dumpstring = dumpstring .. "\n}\n"
 		file:write(dumpstring)
 		file:flush()
