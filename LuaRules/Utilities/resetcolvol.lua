01e75165cf6499a2951dcde0044f6f3ee8053b06
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-09-20 23:44:11 -0400

    add a reset colvol function

---
+++ LuaRules/Utilities/resetcolvol.lua
@@ -0,0 +1,11 @@
+function Spring.Utilities.ResetUnitColVol(unitID)
+	local colvol = UnitDefs[Spring.GetUnitDefID(unitID)].collisionVolume
+	local vtype = colvol.type or "sphere" -- default is sphere, apparently.
+	local axis
+	if vtype == "ellipsoid" or vtype == "sphere" then
+		axis = 0
+	else
+		axis = (vtype:find("%ax") and 0) or (vtype:find("%az") and 2) or (vtype:find("%a%ay") and 1) or 0
+	end
+	Spring.SetUnitCollisionVolumeData(unitID, colvol.scaleX, colvol.scaleY, colvol.scaleZ, colvol.offsetX, colvol.offsetY, colvol.offsetZ, vtype, 1, axis)
+end
