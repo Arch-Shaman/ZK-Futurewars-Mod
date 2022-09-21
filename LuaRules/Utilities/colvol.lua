8af40b9c4efd75c52ed4204380aa5c3f3ae768f3
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-09-21 00:34:04 -0400

    Added a setcolvoloffset func

--- LuaRules/Utilities/colvol.lua
+++ LuaRules/Utilities/colvol.lua
@@ -9,3 +9,15 @@ function Spring.Utilities.ResetUnitColVol(unitID)
 	end
 	Spring.SetUnitCollisionVolumeData(unitID, colvol.scaleX, colvol.scaleY, colvol.scaleZ, colvol.offsetX, colvol.offsetY, colvol.offsetZ, vtype, 1, axis)
 end
+
+function Spring.Utilities.SetUnitColVolOffsets(unitID, x, y, z)
+	local colvol = UnitDefs[Spring.GetUnitDefID(unitID)].collisionVolume
+	local vtype = colvol.type or "sphere" -- default is sphere, apparently.
+	local axis
+	if vtype == "ellipsoid" or vtype == "sphere" then
+		axis = 0
+	else
+		axis = (vtype:find("%ax") and 0) or (vtype:find("%az") and 2) or (vtype:find("%a%ay") and 1) or 0
+	end
+	Spring.SetUnitCollisionVolumeData(unitID, colvol.scaleX, colvol.scaleY, colvol.scaleZ, x, y, z, vtype, 1, axis)
+end
