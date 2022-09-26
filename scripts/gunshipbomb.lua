7a6f3c4be075e50a21c82730b709cd7be83df88a
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-09-26 09:12:39 -0400

    Fix seeker being targetable after firing

--- scripts/gunshipbomb.lua
+++ scripts/gunshipbomb.lua
@@ -113,13 +113,18 @@ local function CleanupThread()
 	Spring.SetUnitNoSelect(unitID, true)
 	Spring.SetUnitNoDraw(unitID, true)
 	Spring.SetUnitNoMinimap(unitID, true)
+	local _, maxHealth = Spring.GetUnitHealth(unitID)
 	Spring.SetUnitHealth(unitID, {paralyze = 99999999, health = maxHealth}) -- also heal to drop (now off-map) repair orders
+	Spring.MoveCtrl.Enable(unitID)
+	Spring.MoveCtrl.SetPosition(unitID, -10000, 0, -10000)
+	Spring.SetUnitRulesParam(unitID, "untargetable", 1, {public = true})
 	Spring.SetUnitCloak(unitID, 4)
 	Spring.SetUnitStealth(unitID, true)
 	Spring.SetUnitBlocking(unitID,false,false,false)
 	Spring.GiveOrderToUnit(unitID, CMD.STOP, 0, 0)
 	
-	Sleep(5000)
+	Sleep(10000)
+	Spring.MoveCtrl.Disable(unitID)
 	Spring.DestroyUnit(unitID, false, true)
 end
 
