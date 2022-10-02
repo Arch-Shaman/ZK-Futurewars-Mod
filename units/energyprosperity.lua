6ce78d1fda7632b817312bd48bf65d6b4c280e1d
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-10-02 18:55:01 -0400

    Mass Restylization pt1

--- units/energyprosperity.lua
+++ units/energyprosperity.lua
@@ -1,19 +1,19 @@
 -- TODO: Organize this shit.
 return {
 	energyprosperity = {
-		unitname = [[energyprosperity]],
+		unitname = "energyprosperity",
 		name = "Prosperity",
 		description = "Economic Superweapon",
 		builder = false,
 		canSelfDestruct = false,
-		category = [[FLOAT UNARMED]],
-		collisionVolumeOffsets = [[0 0 0]],
-		collisionvolumescales = [[90 125 90]],
-		collisionVolumeType = [[Box]],
-		corpse = [[DEAD]],
+		category = "FLOAT UNARMED",
+		collisionVolumeOffsets = "0 0 0",
+		collisionvolumescales = "90 125 90",
+		collisionVolumeType = "Box",
+		corpse = "DEAD",
 		objectname = "pw_mine2.dae",
 		script = "pw_mine2.lua",
-		icontype = [[pw_energy]],
+		icontype = "pw_energy",
 		customparams = {
 			pylonrange = 700,
 			removewait = 1,
@@ -37,24 +37,24 @@ return {
 		footprintx = 7,
 		footprintz = 7,
 		buildcostmetal = 35000,
-		yardMap = [[ooooooo ooooooo ooooooo ooooooo ooooooo ooooooo ooooooo]],
+		yardMap = "ooooooo ooooooo ooooooo ooooooo ooooooo ooooooo ooooooo",
 		sightDistance = 1050,
 		featureDefs                   = {
 			DEAD  = {
 				blocking         = true,
-				featureDead      = [[HEAP]],
+				featureDead      = "HEAP",
 				footprintX       = 5,
 				footprintZ       = 6,
-				object           = [[pw_mine2_dead.dae]],
-				collisionVolumeOffsets        = [[0 0 0]],
-				collisionVolumeScales         = [[90 125 90]],
-				collisionVolumeType           = [[box]],
+				object           = "pw_mine2_dead.dae",
+				collisionVolumeOffsets        = "0 0 0",
+				collisionVolumeScales         = "90 125 90",
+				collisionVolumeType           = "box",
 			},
 			HEAP  = {
 				blocking         = false,
 				footprintX       = 7,
 				footprintZ       = 7,
-				object           = [[debris8x8a.s3o]],
+				object           = "debris8x8a.s3o",
 			},
 		}
 	}
--- units/factorycomm.lua
+++ units/factorycomm.lua
@@ -1,6 +1,6 @@
 -- Idea shamelessly stolen from Tankium's More Units mod.
 -- TODO: 
--- * Replace model at some point or get the weapons to actually work.\\
+-- * Replace model at some point or get the weapons to actually work.
 
 return {
 	factorycomm = {
@@ -8,26 +8,26 @@ return {
 		name = "Commander Chassis Factory",
 		description = "Produces Commanders",
 		objectname = "pw_dropfac.dae",
-		icontype = [[pw_dropfac]],
+		icontype = "pw_dropfac",
 		script = "pw_dropfac.lua",
 		footprintx = 20,
 		footprintz = 16,
 		buildCostMetal                = Shared.FACTORY_COST * 2,
 		buildDistance                 = Shared.FACTORY_PLATE_RANGE,
 		builder                       = true,
-		buildpic			          = [[pw_dropfac.png]],
+		buildpic			          = "pw_dropfac.png",
 		buildingGroundDecalDecaySpeed = 30,
 		buildingGroundDecalSizeX      = 20,
 		buildingGroundDecalSizeY      = 16,
-		category			          = [[UNARMED SINK LAND]],
+		category			          = "UNARMED SINK LAND",
 		customParams = {
 			soundselect = "cpl_select",
 		},
 		energyUse                     = 0,
-		explodeAs                     = [[LARGE_BUILDINGEX]],
+		explodeAs                     = "LARGE_BUILDINGEX",
 		footprintX                    = 20,
 		footprintZ                    = 16,
-		collisionvolumescales         = [[275 120 215]],
+		collisionvolumescales         = "275 120 215",
 		showNanoSpray                 = false,
 		canmove                       = true,
 		canattack                     = true,
@@ -35,26 +35,26 @@ return {
 		maxSlope                      = 15,
 		maxDepth					  = 2500,
 		buildoptions                  = { 		
-			[[dynsupport0]],
-			[[dynrecon0]],
-			[[dynassault0]],
-			[[dynstrike0]],
-			[[dynriot0]],
+			"dynsupport0",
+			"dynrecon0",
+			"dynassault0",
+			"dynstrike0",
+			"dynriot0",
 		},
-		yardmap                       = [[ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo]],
+		yardmap                       = "ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo ooooooooyyyyyyoooooo",
 		featureDefs                   = {
 			DEAD  = {
 				blocking         = true,
-				featureDead      = [[HEAP]],
+				featureDead      = "HEAP",
 				footprintX       = 2,
 				footprintZ       = 2,
-				object           = [[pw_dropfac_dead.dae]],
+				object           = "pw_dropfac_dead.dae",
 			},
 			HEAP  = {
 				blocking         = false,
 				footprintX       = 2,
 				footprintZ       = 2,
-				object           = [[debris8x8b.s3o]],
+				object           = "debris8x8b.s3o",
 			},
 		},
 	},
