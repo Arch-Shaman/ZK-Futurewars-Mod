45e85250ba80bf294cf919ed33c8d704aff08894
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-10-13 00:00:08 -0400

    Added Riptide, the Amphibious Artillery

--- effects/waketrail.lua
+++ effects/waketrail.lua
@@ -21,5 +21,28 @@ return {
 				ttl                = 8,
 			},
 		},
-	}
+	},
+	["waketrail_small"] = {
+		alwaysvisible      = false,
+		usedefaultexplosions = false,
+		largeflash = {
+			air                = true,
+			class              = [[CBitmapMuzzleFlame]],
+			count              = 1,
+			ground             = true,
+			underwater         = 1,
+			water              = true,
+			properties = {
+				colormap           = [[0.239 0.643 0.784 0.01 0.1294 0.4549 0.8313 0.01 0 0 0 0.01]],
+				dir                = [[dir]],
+				frontoffset        = 0,
+				fronttexture       = [[muzzlefront]],
+				length             = -5,
+				sidetexture        = [[muzzleside]],
+				size               = -8,
+				sizegrowth         = 0.75,
+				ttl                = 3,
+			},
+		},
+	},
 }
