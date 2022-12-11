e9e7a531cb4b07845244b0055de2949477691bda
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-12-11 02:51:25 -0500

    Restylize Heavy Disruption Rifle

--- gamedata/modularcomms/weapons/commweapon_heavyrifle_disrupt.lua
+++ gamedata/modularcomms/weapons/commweapon_heavyrifle_disrupt.lua
@@ -1,6 +1,6 @@
 local name = "commweapon_heavyrifle_disrupt"
 local weaponDef = {
-	name                    = [[Heavy Disruption Rifle]],
+	name                    = "Heavy Disruption Rifle",
 	areaOfEffect            = 96,
 	burst                   = 3,
 	burstrate               = 0.2,
@@ -8,10 +8,10 @@ local weaponDef = {
 	craterMult              = 0,
 	customParams            = {
 		is_unit_weapon = 1,
-		muzzleEffectShot = [[custom:beamlaser_violet_muzzle]],
-		miscEffectFire = [[custom:emg_shells_l]],
+		muzzleEffectShot = "custom:beamlaser_violet_muzzle",
+		miscEffectFire = "custom:emg_shells_l",
 		timeslow_damageFactor = 2,
-		light_color = [[1.3 0.5 1.6]],
+		light_color = "1.3 0.5 1.6",
 		light_radius = 120,
 		reaim_time = 1,
 		antibaitbypass = "ärsytät minua",
@@ -20,7 +20,7 @@ local weaponDef = {
 		default = 250.1*.85,
 	},
 	edgeEffectiveness       = 0.5,
-	explosionGenerator      = [[custom:BEAMWEAPON_HIT_PURPLE]],
+	explosionGenerator      = "custom:BEAMWEAPON_HIT_PURPLE",
 	impulseBoost            = 0,
 	impulseFactor           = 0,
 	interceptedByShieldType = 1,
@@ -28,11 +28,11 @@ local weaponDef = {
 	range                   = 320,
 	reloadtime              = 2,
 	rgbColor				= "0.9 0.1 0.9",
-	soundHit                = [[weapon/aoe_aura2]],
-	soundStart              = [[weapon/laser/heavydisruptor]],
+	soundHit                = "weapon/aoe_aura2",
+	soundStart              = "weapon/laser/heavydisruptor",
 	soundstartvolume		= 12,
 	turret                  = true,
-	weaponType              = [[Cannon]],
+	weaponType              = "Cannon",
 	weaponVelocity          = 450,
 }
 
