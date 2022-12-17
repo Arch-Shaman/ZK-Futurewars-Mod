19fc803e9fa036543fe9a76fa0df444a15ef7a31
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-12-17 17:29:42 -0500

    added zombie spawns

--- LuaRules/Gadgets/silly_playerzombies.lua
+++ LuaRules/Gadgets/silly_playerzombies.lua
@@ -18,10 +18,12 @@ if (not gadgetHandler:IsSyncedCode()) then
 	return 
 end
 
-
-local gaiaTeamID = Spring.GetGaiaTeamID()
 local isEnabled = tonumber(Spring.GetModOptions()["playable_zombies"] or "1") == 1
 
+if not isEnabled then return end
+
+local zombieCount = 0
+local gaiaTeamID = Spring.GetGaiaTeamID()
 
 local function ChangeToZombie(playerID)
 	local name = Spring.GetPlayerInfo(playerID)
@@ -36,7 +38,11 @@ function gadget:GameStart()
 		local name, active, spectator = Spring.GetPlayerInfo(playerID)
 		if spectator then
 			ChangeToZombie(playerID)
+			zombieCount = zombieCount + 1
 		end
+	end
+	if zombieCount == 0 then
+		zombieCount = math.random(1, 10)
 	end
 end
 
@@ -46,5 +52,16 @@ function gadget:RecvLuaMsg(msg, playerID)
 		if spectator then
 			ChangeToZombie(playerID)
 		end
+	end
+end
+
+function gadget:GameStart()
+	local randomTable = {"dynsupport0", "dynstrike0", "dynriot0", "dynassault0", "dynrecon0"}
+	local xBound = Game.mapSizeX
+	local yBound = Game.mapSizeZ
+	for i = 1, zombieCount do
+		local x = math.random(xBound * 0.4, xBound * 0.6)
+		local z = math.random(zBound * 0.4, zBound * 0.6)
+		Spring.CreateUnit(randomTable[math.random(1, #randomTable)], x, Spring.GetGroundHeight(x, z), z)
 	end
 end
