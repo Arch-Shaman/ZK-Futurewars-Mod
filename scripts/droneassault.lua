24eb29b6b6e4466d0801d34188caadbd2f7c5edf
LeojEspino <104432653+LeojEspino@users.noreply.github.com>
2023-01-08 00:10:08 +0800

    Python's script

---
+++ scripts/droneassault.lua
@@ -0,0 +1,55 @@
+include "constants.lua"
+
+local body = piece 'body'
+local flare = piece 'flare'
+local wings = piece 'wings'
+local fan = piece 'fan'
+local Rwingengine = piece 'Rwingengine'
+local Lwingengine = piece 'Lwingengine'
+
+local smokePiece = {body}
+
+local spGetUnitVelocity = Spring.GetUnitVelocity
+
+local function TiltWings()
+	while true do
+		if attacking then
+			Turn(wings, x_axis, 0, math.rad(60))
+			Sleep(250)
+		else
+			local vx,_,vz = spGetUnitVelocity(unitID)
+			local speed = vx*vx + vz*vz
+			Turn(wings, x_axis, math.rad(2.5 * speed), math.rad(60))
+			Sleep(250)
+		end
+	end
+end
+
+function script.Create()
+    StartThread(GG.Script.SmokeUnit, unitID, smokePiece)
+	StartThread(TiltWings)
+	Spin(fan, y_axis, math.rad(500))
+end
+
+function script.QueryWeapon(num)
+    return flare
+end
+
+function script.AimFromWeapon(num)
+    return body
+end
+
+function script.AimWeapon(num, heading, pitch)
+	return true
+end
+
+function script.Killed(recentDamage, maxHealth)
+	local severity = (recentDamage/maxHealth)
+	if severity < .5 then
+		Explode(body, SFX.NONE)
+	elseif severity < 1 then
+		Explode(body, SFX.NONE)
+	else
+		Explode(body, SFX.SHATTER)
+	end
+end
