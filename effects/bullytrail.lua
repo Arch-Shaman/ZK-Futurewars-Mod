51360f3efe19d380db92aed0deb0ec56dbcab753
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-01-11 00:52:33 -0500

    Improved Bully Visuals

--- effects/bullytrail.lua
+++ effects/bullytrail.lua
@@ -7,11 +7,12 @@ return {
 			count              = 3,
 			ground             = true,
 			water              = true,
+			underwater         = true,
 			properties = {
 				alpha              = 0.9,
 				alphadecay         = 0.13,
 				alwaysvisible      = false,
-				color              = [[0.1,0.1,1]],
+				color              = [[1,0.1,1]],
 				dir                = [[-4 r8, -4 r8, -4 r8]],
 				length             = 6.5,
 				speed              = [[-0.06 r0.12, -0.06 r0.12, -0.06 r0.12]], -- speed allows for control over lengthgrowth.
@@ -24,11 +25,12 @@ return {
 			count              = 2,
 			ground             = true,
 			water              = true,
+			underwater         = true,
 			properties = {
 				alpha              = 0.95,
 				alphadecay         = 0.13,
 				alwaysvisible      = false,
-				color              = [[0.5,0.5,1]],
+				color              = [[0.5,0,0.5]],
 				dir                = [[-2 r4, -2 r4, -2 r4]],
 				length             = 2.2,
 				--speed              = [[-0.06 r0.12, -0.06 r0.12, -0.06 r0.12]],
