05f34392f80e464313c02437175759ed809f0d1a
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-03-02 13:41:13 -0500

    Add temporary armor gadget

---
+++ LuaRules/Gadgets/unit_armorstates.lua
@@ -0,0 +1,170 @@
+if not gadgetHandler:IsSyncedCode() then -- SYNCED
+	return
+end
+
+function gadget:GetInfo()
+	return {
+		name      = "Armor States",
+		desc      = "Haxy Armors",
+		author    = "Shaman",
+		date      = "02-28-2023",
+		license   = "CC-0",
+		layer     = 0,
+		enabled   = true,
+	}
+end
+
+local IterableMap = VFS.Include("LuaRules/Gadgets/Include/IterableMap.lua")
+local handledUnits = IterableMap.New() -- which units currently have armor. Table structure: {duration = number, armorValue = number}
+local armoredUnits = {} -- table that holds units with armor.
+local watchWeapons = {} -- table that tells PreDamageWantedDefs which weapondefs we want
+local bufferProjectiles = {} -- stores information we may need for ranged units.
+local deleteProjectiles = {} -- deletes from the buffer.
+
+-- configs --
+local MAXARMOR = 0.1
+local CHECKTIME = 3 -- 10 hz or about 100ms
+
+-- Speed ups --
+-- These just make the code run faster.
+
+local spValidUnitID = Spring.ValidUnitID
+local spSetUnitArmored = Spring.SetUnitArmored
+local INLOS = {inlos = true}
+
+-- Config --
+local configs = {}
+for i = 1, #WeaponDefs do -- Iterate through every weapon def. WeaponDefs is an ordered table (meaning we have entries between 1 and some arbitrary number [like 863]
+	local weaponDef = WeaponDefs[i] -- Store the current metatable so we can reference it.
+	if weaponDef.customParams.grants_armor then -- does this weapon grant armor?
+		local armorValue = tonumber(weaponDef.customParams.grants_armor)
+		local armorDuration = math.ceil((tonumber(weaponDef.customParams.armor_duration) or 3) * 30) -- in seconds
+		local impactEnemies = weaponDef.customParams.affects_enemy ~= nil
+		local needsCaching = weaponDef.customParams.needscaching ~= nil -- use this to prevent lua errors on activated abilities.
+		if weaponDef.customParams.armor_duration == nil then
+			Spring.Echo("[ArmorStates]: missing duration for " .. weaponDef.name .. " (ID " .. i .. "). Defaulting to 3s!")
+		end
+		if armorValue and armorDuration then
+			configs[i] = {value = armorValue, duration = armorDuration, alliedOnly = not impactEnemies} -- store the info in the metatable.
+			watchWeapons[#watchWeapons + 1] = i -- Add to watch weapon table so we can filter stuff out we don't need.
+			if needsCaching then
+				Script.SetWatchWeapon(i, true)
+			end
+		else -- something went wrong
+			Spring.Echo("[ArmorStates]: missing armor value for " .. weaponDef.name .. " (ID " .. i .. ").")
+		end
+	end
+end
+
+function GG.SetUnitArmor(unitID, value) -- needed for crab and halberd or other armor changers. You'll need to set this up in unitscript.
+	armoredUnits[unitID] = value
+	if IterableMap.InMap(handledUnits, unitID) then
+		local data = IterableMap.Get(handledUnits, unitID)
+		if value < 1.0 then
+			local newValue = math.max(value * data.armorValue, MAXARMOR)
+			data.armorValue = newValue
+			spSetUnitArmored(unitID, true, newValue)
+		else
+			spSetUnitArmored(unitID, true, data.armorValue)
+		end
+	else
+		if value < 1.0 then
+			spSetUnitArmored(unitID, true, value)
+		else
+			spSetUnitArmored(unitID, false, value)
+		end
+	end
+end
+
+local function CleanUpUnit(unitID)
+	if armoredUnits[unitID] then
+		spSetUnitArmored(unitID, true, armoredUnits[unitID]) -- revert to old armor.
+	else
+		spSetUnitArmored(unitID, false)
+	end
+	IterableMap.Remove(handledUnits, unitID)
+end
+
+local function UpdateArmor(unitID, value, duration)
+	spSetUnitArmored(unitID, true, value)
+	Spring.SetUnitRulesParam(unitID, "temporaryarmor", value, INLOS)
+	Spring.SetUnitRulesParam(unitID, "temporaryarmorduration", duration, INLOS)
+end
+	
+
+local function AddUnit(unitID, value, duration)
+	local data = IterableMap.Get(handledUnits, unitID)
+	if data then
+		if value == data.armorValue then 
+			data.duration = math.max(duration, data.duration)
+			UpdateArmor(unitID, value, data.duration)
+		else
+			local newDuration = (data.duration + duration) / 2 -- average out the duration
+			local newValue = (data.armorValue + value) / 2 -- average out the value
+			UpdateArmor(unitID, newValue, newDuration)
+		end
+	else
+		local data = {duration = duration, data.armorValue}
+		IterableMap.Add(handledUnits, unitID, data)
+	end
+end
+
+function gadget:UnitDestroyed(unitID)
+	if IterableMap.InMap(unitID) then
+		IterableMap.Remove(unitID)
+	end
+	armoredUnits[unitID] = nil
+end
+
+function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam)
+	local armorerTeam = (bufferProjectiles[projectileID] and bufferProjectiles[projectileID].teamID) or attackerTeam
+	if not armorerTeam then return 0, 0 end
+	if (not configs[weaponDefID].alliedOnly) or Spring.AreTeamsAllied(unitTeam, armorerTeam) then
+		local potentialDamage = WeaponDefs[weaponDefID].damages["default"]
+		local mult = (damage / potentialDamage)
+		AddUnit(unitID, configs[weaponDefID].value * mult, configs[weaponDefID].duration * mult)
+	end
+	if bufferProjectiles[projectileID] and not bufferProjectiles[projectileID].willBeDeleted then
+		local f = Spring.GetGameFrame() + (CHECKTIME * 10)
+		if deleteProjectiles[f] then
+			deleteProjectiles[f][#deleteProjectiles[f] + 1] = projectileID
+		else
+			deleteProjectiles[f] = {[1] = projectileID}
+		end
+		bufferProjectiles[projectileID].willBeDeleted = true
+	end
+	return 0, 0
+end
+
+function gadget:UnitPreDamaged_GetWantedWeaponDef() -- only do certain weapons.
+	return watchWeapons
+end
+
+function gadget:ProjectileCreated(proID, proOwnerID, proWeaponDefID)
+	if configs[proWeaponDefID] then 
+		bufferProjectiles[proID] = {teamID = Spring.GetUnitTeam(proOwnerID), ownerID = proOwnerID, willBeDeleted = false}
+	end
+end
+
+function gadget:GameFrame(f)
+	if f%CHECKTIME == 0 then
+		for unitID, data in IterableMap.Iterator(handledUnits) do
+			if spValidUnitID(unitID) then -- protect us from invalid units.
+				data.duration = data.duration - CHECKTIME
+				if data.duration < 0 then
+					CleanUpUnit(unitID)
+				else
+					data.duration = data.duration - CHECKTIME
+					Spring.SetUnitRulesParam(unitID, "temporaryarmorduration", data.duration, INLOS)
+				end
+			end
+		end
+		if deleteProjectiles[f] then
+			for i = 1, #deleteProjectiles do
+				bufferProjectiles[deleteProjectiles[i]] = nil
+			end
+			deleteProjectiles[f] = nil
+		end
+	end
+end
+
