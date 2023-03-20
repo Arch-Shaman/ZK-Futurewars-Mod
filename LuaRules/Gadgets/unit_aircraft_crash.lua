c63c4a56fdbff0ca0d12fb8626c3d1062114ddbd
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-03-20 15:53:12 -0400

    Rewrote aircrash logic to use IterableMap instead of pairs

--- LuaRules/Gadgets/unit_aircraft_crash.lua
+++ LuaRules/Gadgets/unit_aircraft_crash.lua
@@ -10,7 +10,7 @@ function gadget:GetInfo()
   return {
     name      = "Aircraft Crashing",
     desc      = "Handles crashing planes",
-    author    = "KingRaptor",
+    author    = "KingRaptor, Rewritten by Shaman",
     date      = "22 Jan 2011",
     license   = "GNU LGPL, v2.1 or later",
     layer     = 0,
@@ -23,13 +23,14 @@ end
 --------------------------------------------------------------------------------
 local spGetUnitIsStunned	= Spring.GetUnitIsStunned
 local spGetUnitHealth		= Spring.GetUnitHealth
+local IterableMap           = VFS.Include("LuaRules/Gadgets/Include/IterableMap.lua")
 
 local aircraftDefIDs = {}
 
 function gadget:Initialize()
-	for i=1,#UnitDefs do
+	for i = 1, #UnitDefs do
 		if UnitDefs[i].canFly then
-			aircraftDefIDs[i] = UnitDefs[i].health
+			aircraftDefIDs[i] = true
 		end
 	end
 end
@@ -38,18 +39,15 @@ local LOS_ACCESS = {inlos = true}
 local DAMAGE_MEMORY = 60	-- gameframes
 
 
-local recentDamage = {}	-- indexed by unitID
+local recentDamage = IterableMap.New()	-- indexed by unitID
 local gameFrame = 0
 
-function gadget:GameFrame(n)
-	gameFrame = n
-	if n%16 == 0 then
-		for unitID, damList in pairs(recentDamage) do
-			for frame in pairs(damList) do
-				if frame + DAMAGE_MEMORY < n then
-					damList[frame] = nil
-				end
-			end
+function gadget:GameFrame(f)
+	gameFrame = f
+	for _, data in IterableMap.Iterator(recentDamage) do
+		if data.removalFrames[f] then
+			data.damage = data.damage - data.removalFrames[f]
+			data.removalFrames[f] = nil
 		end
 	end
 end
@@ -68,44 +66,43 @@ function gadget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weap
 	if not aircraftDefIDs[unitDefID] then
 		return
 	end
-	if (not recentDamage[unitID]) or select(3, spGetUnitIsStunned(unitID)) then
+	local _, _, inBuild = spGetUnitIsStunned(unitID)
+	if inBuild then
 		return
 	end
 	--Spring.Echo("Plane damaged")
-	recentDamage[unitID][gameFrame] = (recentDamage[unitID][gameFrame] or 0) + damage
-	
-	if spGetUnitHealth(unitID) < 0 then
+	local data = IterableMap.Get(recentDamage, unitID)
+	local health, maxHealth = spGetUnitHealth(unitID)
+	data.damage = data.damage + damage -- store this so we don't have to recalculate it when a plane crashes.
+	if health < 0 and (data.damage / maxHealth) <= 0.5 then
 		--Spring.Echo("Plane shot down")
-		local rDam = 0
-		for frame, recentDamageInstance in pairs(recentDamage[unitID] or {}) do
-			rDam = rDam + recentDamageInstance
+		CallAsUnitIfExists(unitID, "OnStartingCrash") -- tell the LUS that we're crashing (mostly for transports)
+		Spring.SetUnitCrashing(unitID, true)
+		Spring.SetUnitNoSelect(unitID, true)
+		Spring.SetUnitSensorRadius(unitID, "los", 0)
+		Spring.SetUnitSensorRadius(unitID, "airLos", 0)
+		Spring.SetUnitRulesParam(unitID, "crashing", 1, LOS_ACCESS)
+		GG.UpdateUnitAttributes(unitID)
+		if GG.AircraftCrashingDown then
+			GG.AircraftCrashingDown(unitID) --send event to unit_bomber_command.lua to cancel any airpad reservation hold by this airplane
 		end
-		local maxHealth = aircraftDefIDs[unitDefID]
-		local severity = rDam/maxHealth
-		if severity < 0.5 then
-			CallAsUnitIfExists(unitID, "OnStartingCrash") -- tell the LUS that we're crashing (mostly for transports)
-			Spring.SetUnitCrashing(unitID, true)
-			Spring.SetUnitNoSelect(unitID, true)
-			Spring.SetUnitSensorRadius(unitID, "los", 0)
-			Spring.SetUnitSensorRadius(unitID, "airLos", 0)
-			Spring.SetUnitRulesParam(unitID, "crashing", 1, LOS_ACCESS)
-			GG.UpdateUnitAttributes(unitID)
-			-- note that we let the unit keep its airlos
-			if GG.AircraftCrashingDown then
-				GG.AircraftCrashingDown(unitID) --send event to unit_bomber_command.lua to cancel any airpad reservation hold by this airplane
-			end
-		end
-		recentDamage[unitID] = nil	-- no longer need to track this plane
+		IterableMap.Remove(recentDamage, unitID)
+	elseif health > 0 then
+		local removalFrame = gameFrame + DAMAGE_MEMORY
+		data.removalFrames[removalFrame] = (data.removalFrames[removalFrame] or 0) + damage
 	end
 end
 
 function gadget:UnitDestroyed(unitID, unitDefID, unitTeam)
-	recentDamage[unitID] = nil
+	if aircraftDefIDs[unitDefID] then
+		IterableMap.Remove(recentDamage, unitID)
+	end
 end
 
 function gadget:UnitFinished(unitID, unitDefID, unitTeam)
 	if (not aircraftDefIDs[unitDefID]) or select(3, spGetUnitIsStunned(unitID)) then
 		return
 	end
-	recentDamage[unitID] = {}
+	local data = {damage = 0, removalFrames = {}}
+	IterableMap.Add(recentDamage, unitID, data)
 end
