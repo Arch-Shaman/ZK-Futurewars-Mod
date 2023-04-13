674af4560bffdc3d7f2831413f2b2e4c68751324
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-04-13 14:18:12 -0400

    WIP New Overdrive ability for Whirlwind

--- scripts/gunshipraid.lua
+++ scripts/gunshipraid.lua
@@ -14,13 +14,32 @@ include "constants.lua"
 local SIG_RESTORE = 1
 
 local spGetUnitVelocity = Spring.GetUnitVelocity
+local coolingOff = false
+local overdriving = false
+local spSetUnitRulesParam = Spring.SetUnitRulesParam
+local boostSpeed = 1.5
 
 local function TiltWings()
+	local lastSpeed = 0
+	local maxSpeed = (UnitDefs[unitDefID].speed)/30
+	local maxBoosted = maxSpeed * boostSpeed
+	maxSpeed = maxSpeed * maxSpeed
+	maxBoosted = maxBoosted * maxBoosted
+	local speed, mult, vx, vz
+	local turnRate = math.rad(90)
 	while true do
-		local vx,_,vz = spGetUnitVelocity(unitID)
-		local speed = vx*vx + vz*vz
-		Turn (rjet, x_axis, math.rad(2 * speed), math.rad(60))
-		Turn (ljet, x_axis, math.rad(2 * speed), math.rad(60))
+		vx,_,vz,_ = spGetUnitVelocity(unitID)
+		speed = vx*vx + vz*vz
+		if lastSpeed ~= speed then
+			if overdriving then
+				mult = (speed / maxBoosted) * 90
+			else
+				mult = (speed / maxSpeed) * 90
+			end
+			Turn (rjet, x_axis, math.rad(mult), turnRate)
+			Turn (ljet, x_axis, math.rad(mult), turnRate)
+		end
+		lastSpeed = speed
 		Sleep(100)
 	end
 end
@@ -55,12 +74,46 @@ function script.AimWeapon(num, heading, pitch)
 	Turn (gun, y_axis, heading, math.rad(360))
 	Turn (gun, x_axis, -pitch, math.rad(360))
 	StartThread (RestoreAfterDelay)
+	return not coolingOff
+end
+
+function script.BlockShot(num)
+	if coolingOff then return true end
+	if num == 1 and not overdriving then return false end
+	if num == 2 and overdriving then return false end
 	return true
 end
 
-function script.FireWeapon(num)
-	Turn (rjet, x_axis, 0, math.rad(70))
-	Turn (ljet, x_axis, 0, math.rad(70))
+local function OverDriveThread()
+	Sleep(7000)
+	overdriving = false
+	spSetUnitRulesParam(unitID, "selfMoveSpeedChange", 1)
+	spSetUnitRulesParam(unitID, "selfMaxAccelerationChange", 1)
+	spSetUnitRulesParam(unitID, "selfTurnSpeedChange", 1)
+	GG.Sprint.End(unitID)
+	GG.UpdateUnitAttributes(unitID)
+	coolingOff = true
+	local reloadFrame = Spring.GetGameFrame() + 300
+	Spring.SetUnitWeaponState(unitID, 1, "reloadFrame", reloadFrame) -- force the bar to render.
+	Spring.SetUnitWeaponState(unitID, 1, "reloadTime",  10)
+	local isReloading = true
+	while isReloading do
+		EmitSfx(gun, 1025)
+		Sleep(66)
+		isReloading = Spring.GetGameFrame() < Spring.GetUnitWeaponState(unitID, 1, "reloadFrame")
+	end
+	Spring.SetUnitWeaponState(unitID, 1, "reloadTime",  WeaponDefNames["gunshipraid_laser"].reload) -- return to normal.
+	coolingOff = false
+end
+
+function Overdrive()
+	GG.Sprint.Start(unitID, boostSpeed)
+	spSetUnitRulesParam(unitID, "selfMoveSpeedChange", boostSpeed)
+	spSetUnitRulesParam(unitID, "selfTurnSpeedChange", boostSpeed * 2)
+	spSetUnitRulesParam(unitID, "selfMaxAccelerationChange", boostSpeed)
+	GG.UpdateUnitAttributes(unitID)
+	overdriving = true
+	StartThread(OverDriveThread)
 end
 
 function script.Killed(recentDamage, maxHealth)
