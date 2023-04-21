77361156f9e31e18b7228045f04e6497f47dc4a6
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-04-21 12:39:57 -0400

    Finish dynamic ammo cmd defs

--- LuaRules/Configs/ammostateinfo.lua
+++ LuaRules/Configs/ammostateinfo.lua
@@ -7,10 +7,11 @@ for name, id in pairs(cmds) do
 	local customParams = UnitDefNames[unitName].customParams
 	local count = customParams.ammocount
 	stateTypes[id] = count
-	stateInfo[id] = {texture = {}, stateTooltip = {}}
+	stateInfo[id] = {texture = {}, stateTooltip = {}, stateDesc = {}}
 	for i = 1, count do
 		stateInfo[id].texture[i] = customParams["ammotexture_" .. i]
 		stateInfo[id].stateTooltip[i] = customParams["ammoname_" .. i]
+		stateInfo[id].stateDesc[i] = customParams["ammodesc_" .. i]
 	end
 end
 
--- LuaUI/Configs/integral_menu_config.lua
+++ LuaUI/Configs/integral_menu_config.lua
@@ -6,6 +6,8 @@ local buildCmdFactory, buildCmdEconomy, buildCmdDefence, buildCmdSpecial, buildC
 
 local imageDir = 'LuaUI/Images/commands/'
 
+local _, ammoCMDS = VFS.Include("LuaRules/Configs/ammostateinfo.lua")
+
 local tooltips = {
 	WANT_ONOFF = "Activation (_STATE_)\n  Toggles unit abilities such as radar, shield charge, and radar jamming.",
 	UNIT_AI = "Unit AI (_STATE_)\n  Move intelligently in combat.",
@@ -43,6 +45,7 @@ local tooltips = {
 	OVERRECLAIM = "Overreclaim Prevention (_STATE_)\nBlocks constructors from reclaiming when storage is nearly full.",
 	FIRECYCLE = "Spread napalm (_STATE_)\nSets whether this unit should prioritize spreading burning status.",
 	ARMORSTATE = "Hunker (_STATE_)\n Hunker down to reduce damage but lose access to weapons.",
+	AMMOSTATE = "Selected Ammo: (_STATE_)\n(_DESCRIPTION_)",
 }
 
 local tooltipsAlternate = {
@@ -348,6 +351,16 @@ local commandDisplayConfig = {
 		}
 	},
 }
+
+for id, data in pairs(ammoCMDS) do
+	commandDisplayConfig[id] = {
+		texture = data.texture,
+		stateTooltip = {},
+	},
+	for i = 1, #data.stateTooltip do
+		tooltips.AMMOSTATE:gsub("(_STATE_)", data.stateTooltip[i]):gsub("(_DESCRIPTION_)", data.stateDesc[i])
+	end
+end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
