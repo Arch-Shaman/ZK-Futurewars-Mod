1e51e6f17b0eca35bb3c39336eb6be5b86d6c320
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-05-08 23:19:41 -0400

    Newton has more health
    HP: 2k -> 2,6k (HP/C: 10 -> 13)

--- units/turretimpulse.lua
+++ units/turretimpulse.lua
@@ -25,7 +25,7 @@ return {
 		footprintZ                    = 2,
 		iconType                      = "defensesupport",
 		levelGround                   = false,
-		maxDamage                     = 2000,
+		maxDamage                     = 2600,
 		maxSlope                      = 36,
 		maxWaterDepth                 = 0,
 		noAutoFire                    = false,
