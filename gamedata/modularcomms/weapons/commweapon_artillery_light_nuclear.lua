01f5ac190a53b1aff14cc4aad5e2e5533793e6b3
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-09-02 15:31:56 -0400

    Mass restylize

--- gamedata/modularcomms/weapons/commweapon_artillery_light_nuclear.lua
+++ gamedata/modularcomms/weapons/commweapon_artillery_light_nuclear.lua
@@ -1,12 +1,12 @@
 local name = "commweapon_artillery_light_nuclear"
 local weaponDef = {
-	name                    = [[Heavy Rapid Fire Plasma Battery]],
+	name                    = "Heavy Rapid Fire Plasma Battery",
 	accuracy                = 550,
 	areaOfEffect            = 128,
 
 	customParams            = {
 		is_unit_weapon = 1,
-		muzzleEffectFire = [[custom:thud_fire_fx]],
+		muzzleEffectFire = "custom:thud_fire_fx",
 		reaim_time = 1,
 	},
 
@@ -18,17 +18,17 @@ local weaponDef = {
 	},
 
 	edgeEffectiveness       = 0.5,
-	explosionGenerator      = [[custom:MEDMISSILE_EXPLOSION]],
+	explosionGenerator      = "custom:MEDMISSILE_EXPLOSION",
 	impulseBoost            = 0,
 	impulseFactor           = 0,
 	interceptedByShieldType = 1,
 	noSelfDamage            = true,
 	range                   = 800,
 	reloadtime              = 10/30,
-	soundHit                = [[explosion/explosion_roach]],
-	soundStart              = [[weapon/cannon/tremor_fire]],
+	soundHit                = "explosion/explosion_roach",
+	soundStart              = "weapon/cannon/tremor_fire",
 	turret                  = true,
-	weaponType              = [[Cannon]],
+	weaponType              = "Cannon",
 	weaponVelocity          = 420,
 	myGravity               = 0.1,
 }
--- gamedata/modularcomms/weapons/commweapon_leolaser_shotgun_disrupt.lua
+++ gamedata/modularcomms/weapons/commweapon_leolaser_shotgun_disrupt.lua
@@ -1,6 +1,6 @@
 local name = "commweapon_leolaser_shotgun_disrupt"
 local weaponDef = {
-	name                    = [[LEO Disruptor Shotgun]],
+	name                    = "LEO Disruptor Shotgun",
 	areaOfEffect            = 15,
 	avoidFeature            = false,
 	coreThickness           = 0.4,
@@ -16,8 +16,8 @@ local weaponDef = {
 	customParams        = {
 		light_camera_height = 1800,
 		light_radius = 90,
-		light_color = [[0.3 0 0.4]],
-		timeslow_damagefactor = [[3]],
+		light_color = "0.3 0 0.4",
+		timeslow_damagefactor = "3",
 	},
 
 	damage                  = {
@@ -25,29 +25,29 @@ local weaponDef = {
 	},
 
 	duration                = 0.1,
-	explosionGenerator      = [[custom:flash2purple]],
+	explosionGenerator      = "custom:flash2purple",
 	fireStarter             = 50,
 	impulseBoost            = 0,
 	impulseFactor           = 0,
 	interceptedByShieldType = 1,
 	range                   = 280,
 	reloadtime              = 0.3,
-	rgbColor                = [[0.3 0 0.4]],
-	--soundHit                = [[weapon/laser/emerald_hit]],
-	soundStart              = [[weapon/laser/leo_shotgun_disrupt]],
+	rgbColor                = "0.3 0 0.4",
+	--soundHit                = "weapon/laser/emerald_hit",
+	soundStart              = "weapon/laser/leo_shotgun_disrupt",
 	soundStartVolume        = 0.7,
 	soundTrigger            = false,
 	thickness               = 1.1,
 	tolerance               = 10000,
 	turret                  = true,
 	largebeamlaser			= true,
-	texture1                = [[lightlaser]],
-	texture2                = [[flare]],
-	texture3                = [[flare]],
+	texture1                = "lightlaser",
+	texture2                = "flare",
+	texture3                = "flare",
 	beamDecay 				= 0.8,
 	beamBurst				= true,
 	beamTTL					= 13,
-	weaponType              = [[BeamLaser]],
+	weaponType              = "BeamLaser",
 	--weaponVelocity          = 880,
 }
 
