8ca115a75625e05139518b7496b0cadcd703882f
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-09-08 06:17:06 -0400

    Fix dead wielder crash

--- LuaUI/Widgets/gui_contextmenu.lua
+++ LuaUI/Widgets/gui_contextmenu.lua
@@ -1511,7 +1511,7 @@ local function printAbilities(ud, unitID, isFeature)
 	if ud.buildSpeed > 0 and not cp.nobuildpower then
 		local bpMult = 1
 		if isFeature then
-			bpMult = Spring.GetFeatureRulesParam(unitID, "buildpower_mult") or 1
+			bpMult = unitID and Spring.GetFeatureRulesParam(unitID, "buildpower_mult") or 1
 		elseif unitID and Spring.GetUnitRulesParam(unitID, "comm_level") then
 			bpMult = unitID and Spring.GetUnitRulesParam(unitID, "buildpower_mult") or 1
 		end
