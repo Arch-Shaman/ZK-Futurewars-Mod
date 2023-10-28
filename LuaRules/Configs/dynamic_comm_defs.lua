a2683bad553587b98c1363f4c0dab485f007b247
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-10-27 22:10:38 -0400

    Add some new modules for strike's shotguns

--- LuaRules/Configs/dynamic_comm_defs.lua
+++ LuaRules/Configs/dynamic_comm_defs.lua
@@ -1580,6 +1580,78 @@ local moduleDefs = {
 		end
 	},
 	{
+		name = "module_improved_choke",
+		humanName = "Improved Choke",
+		description = "Improved Choke:\nImproves shotgun accuracy at the cost of damage output. Better performance for long range.\nShotguns:\nSpread: -30%\nprojectiles -4",
+		image = moduleImagePath .. "module_improved_choke.png",
+		limit = 3,
+		cost = 100 * COST_MULT,
+		requireChassis = {"strike"},
+		requireLevel = 3,
+		slotType = "module",
+		prohibitingModules = {"module_full_choke"},
+		requireOneOf = {"commweapon_shotgun"},
+		applicationFunction = function (modules, sharedData)
+			if sharedData.weapon1 and (sharedData.weapon1 == "commweapon_shotgun" or sharedData.weapon1 == "commweapon_shotgun_disrupt") then
+				sharedData.sprayAngleBonus1 = (sharedData.sprayAngleBonus1 or 0) - 0.3
+				sharedData.accuracyBonus1 = (sharedData.accuracyBonus1 or 0) + 0.3
+				sharedData.projectileBonus1 = (sharedData.projectileBonus1 or 0) - 4
+			end
+			if sharedData.weapon2 and (sharedData.weapon2 == "commweapon_shotgun" or sharedData.weapon2 == "commweapon_shotgun_disrupt") then
+				sharedData.sprayAngleBonus2 = (sharedData.sprayAngleBonus2 or 0) - 0.3
+				sharedData.accuracyBonus2 = (sharedData.accuracyBonus2 or 0) + 0.3
+				sharedData.projectileBonus2 = (sharedData.projectileBonus2 or 0) - 4
+			end
+		end
+	},
+	{
+		name = "module_full_choke",
+		humanName = "Flechette Engineering",
+		description = "Flechette Engineering:\nImproves damage output for shotguns at the cost of higher spread.\nShotguns:\nSpread: +33%\nprojectiles +5",
+		image = moduleImagePath .. "module_full_choke.png",
+		limit = 3,
+		cost = 100 * COST_MULT,
+		requireChassis = {"strike"},
+		requireLevel = 3,
+		slotType = "module",
+		requireOneOf = {"commweapon_shotgun"},
+		prohibitingModules = {"module_improved_choke"},
+		applicationFunction = function (modules, sharedData)
+			if sharedData.weapon1 and (sharedData.weapon1 == "commweapon_shotgun" or sharedData.weapon1 == "commweapon_shotgun_disrupt") then
+				sharedData.sprayAngleBonus1 = (sharedData.sprayAngleBonus1 or 0) + 0.25
+				sharedData.accuracyBonus1 = (sharedData.accuracyBonus1 or 0) + 0.25
+				sharedData.projectileBonus1 = (sharedData.projectileBonus1 or 0) + 5
+			end
+			if sharedData.weapon2 and (sharedData.weapon2 == "commweapon_shotgun" or sharedData.weapon2 == "commweapon_shotgun_disrupt") then
+				sharedData.sprayAngleBonus2 = (sharedData.sprayAngleBonus2 or 0) + 0.25
+				sharedData.accuracyBonus2 = (sharedData.accuracyBonus2 or 0) + 0.25
+				sharedData.projectileBonus2 = (sharedData.projectileBonus2 or 0) + 5
+			end
+		end
+	},
+	{
+		name = "module_autoflechette",
+		humanName = "Autoflechette",
+		description = "Autoflechette:\nShotguns: Increase reload speed by 33%, but fire 5 less flechettes.",
+		image = moduleImagePath .. "module_autoflechette.png",
+		limit = 1,
+		cost = 100 * COST_MULT,
+		requireOneOf = {"commweapon_shotgun"},
+		requireChassis = {"strike"},
+		requireLevel = 2,
+		slotType = "module",
+		applicationFunction = function (modules, sharedData)
+			if sharedData.weapon1 and (sharedData.weapon1 == "commweapon_shotgun" or sharedData.weapon1 == "commweapon_shotgun_disrupt") then
+				sharedData.reloadOverride1 = 26/30
+				sharedData.projectileBonus1 = (sharedData.projectileBonus1 or 0) - 5
+			end
+			if sharedData.weapon2 and (sharedData.weapon2 == "commweapon_shotgun" or sharedData.weapon2 == "commweapon_shotgun_disrupt") then
+				sharedData.reloadOverride2 = 26/30
+				sharedData.projectileBonus2 = (sharedData.projectileBonus2 or 0) - 5
+			end
+		end
+	},
+	{
 		name = "module_repair_drone",
 		humanName = "Repair Drone",
 		description = "Adds a Repair Drone to your maximum drone control. Repair drones have a shield and can repair friendly units at 10 bp.",
