cb909c4fecc3f30cf5c928bf3bc9ebfbdf959883
IdkSomethingTemporary <113983840+IdkSomethingTemporary@users.noreply.github.com>
2024-01-03 12:06:44 +1100

    Make pyro use hack

--- units/jumpraid.lua
+++ units/jumpraid.lua
@@ -101,7 +101,7 @@ return {
 				coreThickness           = 0,
 				craterBoost             = 0,
 				craterMult              = 0,
-				cegTag                  = "napalmtrail_halfsize",
+				cegTag                  = "flamer",
 				customParams            = {
 					flamethrower = "1",
 					setunitsonfire = "1",
@@ -155,7 +155,7 @@ return {
 				coreThickness           = 0,
 				craterBoost             = 0,
 				craterMult              = 0,
-				cegTag                  = "napalmtrail_halfsize",
+				cegTag                  = "flamer",
 				customParams            = {
 					flamethrower = "1",
 					setunitsonfire = "1",
