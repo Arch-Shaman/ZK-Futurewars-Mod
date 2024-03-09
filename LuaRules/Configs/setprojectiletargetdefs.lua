f522b7030e34faaed9966f095e6c1d30369d65a7
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2024-03-08 19:24:56 -0500

    [Ignore] Fix #606

--- LuaRules/Configs/setprojectiletargetdefs.lua
+++ LuaRules/Configs/setprojectiletargetdefs.lua
@@ -1,14 +1,14 @@
 local wantedefs = {}
 
 do
-	local nukedef = UnitDefNames["staticnuke"].id
-	local nukesubdef = UnitDefNames["subtacmissile"].id
-	local nukecp = WeaponDefs[nukedef.weapons[1].id].customParams
-	local subcp = WeaponDefs[nukesubdef.weapons[1].id].customParams
+	local nukedef = UnitDefNames["staticnuke"]
+	local nukesubdef = UnitDefNames["subtacmissile"]
+	local nukecp = WeaponDefs[nukedef.weapons[1].weaponDef].customParams
+	local subcp = WeaponDefs[nukesubdef.weapons[1].weaponDef].customParams
 	local nukeRange = tonumber(nukecp.mirvrange) or 2500
 	local subRange = tonumber(subcp.mirvrange) or 500
 	wantedefs[nukedef.id] = {targets = tonumber(nukecp.numprojectiles1) or 5, range = nukeRange, noTargetRange = nukeRange/5, UIaoe = 800}
-	wantedefs[UnitDefNames["subtacmissile"].id] = {targets = tonumber(subcp.numprojectiles1) or 5, range = subRange, noTargetRange = subRange/5, UIaoe = 80}
+	wantedefs[nukesubdef.id] = {targets = tonumber(subcp.numprojectiles1) or 5, range = subRange, noTargetRange = subRange/5, UIaoe = 80}
 end
 
 for index, data in pairs(wantedefs) do
