a2470b305dd896521728980d153822e677af4afd
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2024-03-09 02:35:08 -0500

    Remodel of Oblivion by DanWarrior
    This new and shiny launch site now awaits your command to send MIRVs at the enemy bases. Thanks to DanWarrior for the model!

--- scripts/staticnuke_mirv.lua
+++ scripts/staticnuke_mirv.lua
@@ -1,131 +1,148 @@
-local base = piece 'base'
---local tube = piece 'hatch'
-local door1 = piece 'door.b'
-local door2 = piece 'door.f'
-local door3 = piece 'door.l'
-local door4 = piece 'door.r'
-local launcher = piece 'launcher'
-local missile = piece 'mirv'
-local emit1 = piece 'emit1'
-local emit2 = piece 'emit2'
-local flare = piece 'flare'
-
--- unused pieces: mirvdoor1, mirvdoor2, mirvdoor3
+local base = piece 'Base'
+local hatch = piece 'Hatch'
+local door1 = piece 'Door1'
+local door2 = piece 'Door2'
+local door3 = piece 'Door3'
+local door4 = piece 'Door4'
+local launcher = piece 'Launcher'
+local missile = piece 'Missile'
+local emit1 = piece 'Emit1'
+local mirv1 = piece 'MirvDoor1'
+local mirv2 = piece 'MirvDoor2'
+local mirv3 = piece 'MirvDoor3'
 
 include "constants.lua"
 
-local RESTORE_DELAY = 3000
 local openingDoors = false
 local doorsAreOpen = false
 local closingDoors = false
-local missileLoaded = true
-local primingQueued = false
+local missileLoaded = false
+local priming = false
 local mystock = 0
 
 -- Signal definitions
 local SIG_AIM = 1
-local SIG_RESTORE = 2
+local SIG_DOORS = 4
 
-local function OpenDoors()
-	SetSignalMask(0)
-	
-	if openingDoors or closingDoors then
-		return
-	end
+local function ShowMissile()
+	Show(missile)
+	Show(mirv1)
+	Show(mirv2)
+	Show(mirv3)
+end
+
+local function HideMissile()
+	Hide(missile)
+	Hide(mirv1)
+	Hide(mirv2)
+	Hide(mirv3)
+end
+
+local function CloseHatch()
+	Signal(SIG_DOORS)
+	SetSignalMask(SIG_DOORS)
+	closingDoors = true
+	Move(door1, x_axis, -0.082177, 3.0310885)
+	Move(door1, y_axis, 0.39754, 3.29252)
+	Move(door1, z_axis, 0.095061, 1.2714305)
+	Move(door2, x_axis, 0, 2.99)
+	Move(door2, y_axis, 0.39754, 3.29252)
+	Move(door2, z_axis, 0.082242, 1.265021)
+	Move(door3, x_axis, -0.082177, 3.0310885)
+	Move(door3, y_axis, 0.397554, 3.292527)
+	Move(door3, z_axis, 0.082242, 1.265001)
+	Move(door4, x_axis, -0.094997, 3.0462515)
+	Move(door4, y_axis, 0.095061, 3.0375305)
+	Move(door4, z_axis, 0.39754, 1.42265)
+	Sleep(2100)
+	closingDoors = false
+	doorsAreOpen = false
+end
+
+local function ResetNukePosition()
+	Move(missile, z_axis, 0, 12.61) --37.83
+	Move(launcher, z_axis, 0, 14.97) --44.91
+end
+
+local function MoveNukeToPosition()
+	Move(missile, z_axis, 37.83, 12.61)
+	Move(launcher, z_axis, 44.91, 14.97)
+	WaitForMove(launcher, z_axis)
+	missileLoaded = true
+	priming = false
+end
+
+local function OpenHatch()
+	Signal(SIG_DOORS)
+	SetSignalMask(SIG_DOORS)
 	openingDoors = true
-
-	Move(doorl, x_axis, 0)
-	Move(doorl, x_axis, -22, 14)
-	Move(doorr, x_axis, 0)
-	Move(doorr, x_axis, 22, 14)
-	Move(tube, y_axis, 0)
-	Move(nuke, x_axis, 0)
-	Move(tower, y_axis, 0)
-	
-	Show(tube)
-	Show(tower)
-	Show(nuke)
-	
-	Sleep(1000)
-
-	Move(tube, y_axis, 15, 10)
-	
-	Move(tower, y_axis, 62, 22)
-	Sleep(3000)
+	Move(door1, x_axis, 5.98, 3.0310885)
+	Move(door1, y_axis, -6.1875, 3.29252)
+	Move(door1, z_axis, -2.4478, 1.2714305)
+	Move(door2, x_axis, -5.98, 2.99) -- target 2s
+	Move(door2, y_axis, 6.1875, 3.29252)
+	Move(door2, z_axis, -2.4478, 1.265021)
+	Move(door3, x_axis, 5.98, 3.0310885)
+	Move(door3, y_axis, 6.1875, 3.292527)
+	Move(door3, z_axis, -2.44776, 1.265001)
+	Move(door4, x_axis, -6.1875, 3.0462515)
+	Move(door4, y_axis, -5.98, 3.0375305)
+	Move(door4, z_axis, -2.44776, 1.42265)
+	Sleep(2100)
 	doorsAreOpen = true
 	openingDoors = false
 end
 
-local function CloseDoors()
-	SetSignalMask(0)
-	
-	if openingDoors or closingDoors then
-		return
+local function PrimeNuke()
+	priming = true
+	ShowMissile()
+	StartThread(OpenHatch)
+	while not doorsAreOpen do
+		Sleep(100)
 	end
-	doorsAreOpen = false
-	closingDoors = true
-	
-	Sleep(500)
-	
-	Move(tower, y_axis, 0, 30)
-	Sleep(1000)
-	Move(tube, y_axis, 0, 15)
-	Sleep(1000)
+	StartThread(MoveNukeToPosition)
+end
 
-	Move(doorl, x_axis, 0, 14)
-	Move(doorr, x_axis, 0, 14)
-	Sleep(500)
-	
-	WaitForMove(doorr, x_axis)
-	Hide(tube)
-	
-	Sleep(500)	-- keep door from instantly opening after closing
-	closingDoors = false
-	missileLoaded = true
-	
-	if primingQueued or Spring.GetUnitStockpile(unitID) > 0 then
-		primingQueued = false
-		StartThread(OpenDoors)
+local function FireNukeThread()
+	HideMissile()
+	Sleep(4000)
+	ResetNukePosition()
+	Sleep(3000)
+	StartThread(CloseHatch)
+	while doorsAreOpen do
+		Sleep(100)
+	end
+	missileLoaded = false
+	if mystock > 0 then -- we still have ammo
+		priming = true
+		StartThread(PrimeNuke)
 	end
 end
+
 
 function StockpileChanged(newStock)
 	mystock = newStock
 	if newStock <= 0 then
 		return
 	end
-	if not missileLoaded then
-		primingQueued = true
-	elseif not doorsAreOpen and not openingDoors and not closingDoors then
-		StartThread(OpenDoors)
+	if not missileLoaded and not priming then
+		priming = true
+		StartThread(PrimeNuke)
 	end
 end
 
 function script.Create()
-	StartThread(GG.Script.SmokeUnit, unitID, {piece("smoke1", "smoke2", "smoke3")})
-	Hide(tube)
-	Hide(tower)
-	Hide(nuke)
-end
-
-local function RestoreAfterDelay()
-	Signal(SIG_RESTORE)
-	SetSignalMask(SIG_RESTORE)
-	Sleep(RESTORE_DELAY)
-	
-	StartThread(CloseDoors)
+	StartThread(GG.Script.SmokeUnit, unitID, {hatch, base})
+	HideMissile()
 end
 
 function script.AimWeapon(num, heading, pitch)
-	StartThread(RestoreAfterDelay)
-	
-	if not (missileLoaded or closingDoors) then
-		StartThread(CloseDoors)
-	elseif not (doorsAreOpen or openingDoors) then
-		StartThread(OpenDoors)
-	end
+	if (not missileLoaded) or mystock == 0 then return false end
 	Signal(SIG_AIM)
 	SetSignalMask(SIG_AIM)
+	if not priming and not doorsAreOpen and not missileLoaded and mystock > 0 then
+		StartThread(PrimeNuke)
+	end
 	while not doorsAreOpen do
 		Sleep(25)
 	end
@@ -133,9 +150,7 @@ function script.AimWeapon(num, heading, pitch)
 end
 
 function script.FireWeapon()
-	Hide(nuke)
-	missileLoaded = false
-	doorsAreOpen = false
+	StartThread(FireNukeThread)
 	if GG.GameRules_NukeLaunched then
 		GG.GameRules_NukeLaunched(unitID)
 	end
@@ -145,7 +160,7 @@ function script.FireWeapon()
 end
 
 function script.QueryWeapon()
-	return point
+	return emit1
 end
 
 function script.Killed(recentDamage, maxHealth)
@@ -163,29 +178,16 @@ function script.Killed(recentDamage, maxHealth)
 			end
 		end
 	end
-	if (severity <= .25) then
-		Explode(base, SFX.NONE)
-		Explode(tube, SFX.NONE)
-		Explode(doorl, SFX.NONE)
-		Explode(doorr, SFX.NONE)
-		Explode(tower, SFX.NONE)
-		Explode(nuke, SFX.NONE)
-		return 1
-	elseif (severity <= .5) then
-		Explode(base, SFX.NONE)
-		Explode(tube, SFX.SHATTER)
-		Explode(doorl, SFX.FALL)
-		Explode(doorr, SFX.FALL)
-		Explode(tower, SFX.NONE)
-		Explode(nuke, SFX.NONE)
-		return 1
-	else
-		Explode(base, SFX.NONE)
-		Explode(tube, SFX.SHATTER)
-		Explode(doorl, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
-		Explode(doorr, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT)
-		Explode(tower, SFX.NONE)
-		Explode(nuke, SFX.NONE)
+	local explodeOnHit = SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE_ON_HIT
+	Explode(base, SFX.NONE)
+	if severity >= 0.5 then
+		Explode(door1, explodeOnHit)
+		Explode(door2, explodeOnHit)
+		Explode(door3, explodeOnHit)
+		Explode(door4, explodeOnHit)
+		Explode(launcher, explodeOnHit)
 		return 2
+	else
+		return 1
 	end
 end
