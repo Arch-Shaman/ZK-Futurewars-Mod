7c7930ae770c8b9c0a794a7e69fae8ebcca9b05c
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2024-04-19 11:23:06 -0400

    Lynx
    - Beam time reduced to 2 frames
    - Aim delay: 2s -> 1.5s
    - Increased tolerance a bit.

--- units/spideraa.lua
+++ units/spideraa.lua
@@ -63,19 +63,19 @@ return {
 				avoidGround  = false,
 				canAttackGround         = false,
 				beamDecay               = 0.9,
-				beamTime                = 6/30,
+				beamTime                = 2/30,
 				beamTTL                 = 30,
 				craterBoost             = 0,
 				craterMult              = 0,
 				coreThickness           = 0.9,
 				customParams            = {
 					burst = Shared.BURST_RELIABLE,
-					aimdelay = 60, -- 2 seconds
+					aimdelay = 45, -- 2 seconds
 					light_color = "0.203 0.631 0.196",
 					light_radius = 320,
 					reveal_unit = 8,
-					allowedpitcherror = 30,
-					allowedheadingerror = 90,
+					allowedpitcherror = 90,
+					allowedheadingerror = 120,
 					isaa = "1",
 					aimdelayresettime = 20,
 				},
