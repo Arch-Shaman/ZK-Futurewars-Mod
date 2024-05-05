6963987f6ddddcb7b1d01ffd742a4ea33854ce9f
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2024-05-04 23:19:55 -0400

    Surge
    Speed: 2.6 -> 2.9

--- units/amphraid.lua
+++ units/amphraid.lua
@@ -27,7 +27,7 @@ return {
 		leaveTracks            = true,
 		health                 = 480,
 		maxSlope               = 36,
-		speed                  = 2.6,
+		speed                  = 2.9,
 		maxWaterDepth          = 5000,
 		movementClass          = "AKBOT2",
 		noChaseCategory        = "TERRAFORM SATELLITE FIXEDWING GUNSHIP HOVER SHIP SWIM SUB LAND FLOAT SINK TURRET",
