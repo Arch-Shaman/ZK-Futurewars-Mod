19c5ca0b718678431a5b3689504d7066b241a851
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2024-06-10 09:26:59 -0400

    Update Hella's model to use the new version
    This preempts ZK:stable.

--- LuaUI/Configs/lupsUnitFXs.lua
+++ LuaUI/Configs/lupsUnitFXs.lua
@@ -227,9 +227,28 @@ effectUnitDefs = {
 	-- length tag does nothing
 	--// PLANES //----------------------------
 	bomberheavy = {
-		{class='AirJet', options={color={0.4,0.1,0.8}, width=3.5, length=30, piece="nozzle1", onActive=true, noIconDraw = true}},
-		{class='AirJet', options={color={0.4,0.1,0.8}, width=3.5, length=30, piece="nozzle2", onActive=true, noIconDraw = true}},
-	 },
+		-- jets
+		{class='AirJet', options={color={0.4,0.1,0.8}, width=3.4, length=35, piece="thrust_L", onActive=true, noIconDraw = true, }},
+		{class='AirJet', options={color={0.4,0.1,0.8}, width=3.4, length=35, piece="thrust_R", onActive=true, noIconDraw = true, }},
+
+		-- ribbons
+		{class='Ribbon', options={width=0.5, size=10, piece="wingtip_L", noIconDraw = true}},
+		{class='Ribbon', options={width=0.5, size=10, piece="wingtip_R", noIconDraw = true}},
+
+		-- portable singularity
+		--{class='Bursts', options=MergeTable(energysinguBursts,{piece="ball", noIconDraw = true, size=5, pos={0,0,0}})},
+		--{class='StaticParticles', options=MergeTable(energysinguCorona,{piece="ball", noIconDraw = true, size=18, pos={0,0,0}})},
+
+		-- blinky lights
+		{class='StaticParticles', options=MergeTable(blinkyLightRed,   {piece="extra_L"}) },
+		{class='StaticParticles', options=MergeTable(blinkyLightGreen, {piece="extra_R"}) },
+
+		-- radiator
+		{class='AirJet', options={color={1,0.8,0.4}, width=2.5, length=2.2, piece="rad_L", noIconDraw = true, }},
+		{class='AirJet', options={color={1,0.8,0.4}, width=2.5, length=2.2, piece="rad_R", noIconDraw = true,}},
+		{class='StaticParticles', options=MergeTable(energysinguCorona,{piece="rad_L", noIconDraw = true, size=8.8, pos={0,0,0}})},
+		{class='StaticParticles', options=MergeTable(energysinguCorona,{piece="rad_R", noIconDraw = true, size=8.8, pos={0,0,0}})},
+	},
 	--[[armhawk2 = {
 		{class='AirJet', options={color={0.2,0.2,1.0}, width=2.8, length=25, piece="enginel", onActive=true, noIconDraw = true}},
 		{class='AirJet', options={color={0.2,0.2,1.0}, width=2.8, length=25, piece="enginer", onActive=true, noIconDraw = true}},
--- scripts/bomberheavy.lua
+++ scripts/bomberheavy.lua
@@ -3,30 +3,27 @@ include "bombers.lua"
 include "fixedwingTakeOff.lua"
 
 
-local flare1 = piece 'flare1'
-local flare2 = piece 'flare2'
-local base = piece 'base'
-local wing1 = piece 'wing1'
-local wing2 = piece 'wing2'
-local rearthrust = piece 'rearthrust'
-local wingthrust1 = piece 'wingthrust1'
-local wingthrust2 = piece 'wingthrust2'
-local thrust1 = piece 'thrust1'
-local thrust2 = piece 'thrust2'
-local drop = piece 'drop'
-local emit1 = piece 'emit1'
-local emit2 = piece 'emit2'
-local emit3 = piece 'emit3'
-local emit4 = piece 'emit4'
+local base       = piece 'base'
+local wing_L     = piece 'wing_L'
+local wing_R     = piece 'wing_R'
+local drop       = piece 'drop'
+local extra_L    = piece 'extra_L'
+local extra_R    = piece 'extra_R'
+local radiator_L = piece 'radiator_L'
+local radiator_R = piece 'radiator_R'
+local rad_L      = piece 'rad_L' -- empty piece for fx
+local rad_R      = piece 'rad_R'
+local hatch_L    = piece 'hatch_L'
+local hatch_R    = piece 'hatch_R'
+local ball       = piece 'ball'
+local thrust_L, thrust_R = piece('thrust_L', 'thrust_R')
 
-local smokePiece = {base}
+local smokePiece = {base, radiator_L, radiator_R}
 
 --Signal
 local SIG_move = 1
 local SIG_TAKEOFF = 2
 local takeoffHeight = UnitDefNames["bomberheavy"].wantedHeight
-
-local gun_1 = false
 
 local armedspeed = 1.0
 local unarmedspeed = 1 + 1/3
@@ -38,6 +35,9 @@ local spSetUnitRulesParam = Spring.SetUnitRulesParam
 local movectrlGetTag = Spring.MoveCtrl.GetTag
 local deathexplosiontriggered = false
 local ammoState = 0
+
+local armed = true
+local cooling = false
 
 local function DeathExplosion()
 	if ammoState == 0 then
@@ -54,17 +54,112 @@ local function DeathExplosion()
 	end
 end
 
+local function UpdateCooling()
+	if not armed and not cooling then
+		Show(radiator_L)
+		Show(radiator_R)
+		Turn(hatch_L, y_axis, math.rad(-90), 2)
+		Turn(hatch_R, y_axis, math.rad( 90), 2)
+		Move(radiator_L, z_axis, 3, 1)
+		Move(radiator_R, z_axis, 3, 1)
+		Move(rad_L, z_axis, 3, 1)
+		Move(rad_R, z_axis, 3, 1)
+		--Spin(ball, y_axis, 0)
+		cooling = true
+	end
+
+	if armed and cooling then
+		Move(radiator_L, z_axis, 0, 2)
+		Move(radiator_R, z_axis, 0, 2)
+		Move(rad_L, z_axis, -2, 2)
+		Move(rad_R, z_axis, -2, 2)
+		Turn(hatch_L, y_axis, math.rad(0), 1)
+		Turn(hatch_R, y_axis, math.rad(0), 1)
+		--Spin(ball, y_axis, math.rad(30))
+		WaitForTurn (hatch_L, y_axis)
+		WaitForTurn (hatch_R, y_axis)
+		Hide(radiator_L)
+		Hide(radiator_R)
+		cooling = false
+	end
+end
+
+
+
 function OnAmmoChange(newState)
 	ammoState = newState
 	if newState == 0 then
 		spSetUnitRulesParam(unitID, "selfMoveSpeedChange", 1)
 		spSetUnitRulesParam(unitID, "selfMaxAccelerationChange", 1)
+		armed = true
 		GG.UpdateUnitAttributes(unitID)
 	elseif newState == 1 then
 		spSetUnitRulesParam(unitID, "selfMoveSpeedChange", unarmedspeed)
 		spSetUnitRulesParam(unitID, "selfMaxAccelerationChange", unarmedspeed)
+		armed = false
 		GG.UpdateUnitAttributes(unitID)
 	end
+	--UpdateCooling()
+end
+
+local function Fly()
+	Move(wing_L, x_axis, 0, 6)
+	Move(wing_R, x_axis, 0, 6)
+	Move(wing_L, y_axis, 0, 8)
+	Move(wing_R, y_axis, 0, 8)
+
+	Move(extra_L, x_axis, 0, 3)
+	Move(extra_R, x_axis, 0, 3)
+	Turn(extra_L, z_axis, math.rad(-30), 2)
+	Turn(extra_R, z_axis, math.rad( 30), 2)
+	WaitForTurn (extra_L, z_axis)
+	WaitForTurn (extra_R, z_axis)
+
+	Turn(extra_L, z_axis, math.rad(0), 1)
+	Turn(extra_R, z_axis, math.rad(0), 1)
+end
+
+local function WingStart()
+	Move(wing_L, x_axis, -7, 0)
+	Move(wing_R, x_axis,  7, 0)
+	Move(wing_L, y_axis, -9, 0)
+	Move(wing_R, y_axis, -9, 0)
+
+	Turn(extra_L, z_axis, math.rad(-30), 0)
+	Turn(extra_R, z_axis, math.rad( 30), 0)
+	WaitForTurn (extra_L, z_axis)
+	WaitForTurn (extra_R, z_axis)
+
+	Turn(extra_L, z_axis, math.rad(-146.3), 0)
+	Turn(extra_R, z_axis, math.rad( 146.3), 0)
+	WaitForTurn (extra_L, z_axis)
+	WaitForTurn (extra_R, z_axis)
+
+	Move(extra_L, x_axis, -1, 0)
+	Move(extra_R, x_axis,  1, 0)
+end
+
+local function Land()
+	Turn(extra_L, z_axis, math.rad(-30), 3)
+	Turn(extra_R, z_axis, math.rad( 30), 3)
+
+	WaitForTurn (extra_L, z_axis)
+	WaitForTurn (extra_R, z_axis)
+
+	Turn(extra_L, z_axis, math.rad(-146.3), 2)
+	Turn(extra_R, z_axis, math.rad( 146.3), 2)
+
+	Move(wing_L, x_axis, -7, 6)
+	Move(wing_R, x_axis,  7, 6)
+
+	Move(wing_L, y_axis, -9, 8)
+	Move(wing_R, y_axis, -9, 8)
+
+	WaitForTurn (extra_L, z_axis)
+	WaitForTurn (extra_R, z_axis)
+
+	Move(extra_L, x_axis, -1, 3)
+	Move(extra_R, x_axis,  1, 3)
 end
 
 function OnStartingCrash()
@@ -74,23 +169,13 @@ end
 local function Stopping()
 	Signal(SIG_move)
 	SetSignalMask(SIG_move)
-	Move(wing1, x_axis, -0, 1.65)
-	Move(wing1, z_axis, 0, 0.35)
-	Move(wing2, x_axis, 0, 1.65)
-	Move(wing2, z_axis, 0, 0.35)
-	Turn(wing1, z_axis, 0, math.rad(0.62))
-	Turn(wing2, z_axis, 0, math.rad(1.85))
+	Land()
 end
 
 local function Moving()
 	Signal(SIG_move)
 	SetSignalMask(SIG_move)
-	Move(wing1, x_axis, 2.4, 1.65)
-	Move(wing1, z_axis, -0.5, 0.35)
-	Move(wing2, x_axis, -2.4, 1.65)
-	Move(wing2, z_axis, -0.5, 0.35)
-	Turn(wing1, z_axis, math.rad(-2.7), math.rad(1.85))
-	Turn(wing2, z_axis, math.rad(-2.7), math.rad(1.85))
+	Fly()
 end
 
 function script.StartMoving()
@@ -104,8 +189,6 @@ end
 
 function script.MoveRate(rate)
 	if rate == 1 then
-		--Signal(SIG_BARREL)
-		--SetSignalMask(SIG_BARREL)
 		Turn(base, z_axis, math.rad(-240), math.rad(120))
 		WaitForTurn(base, z_axis)
 		Turn(base, z_axis, math.rad(-(120)), math.rad(180))
@@ -115,19 +198,28 @@ function script.MoveRate(rate)
 end
 
 function script.Create()
+	Move(thrust_L, y_axis, -5)
+	Move(thrust_R, y_axis, -5)
+
+	Turn(thrust_L, x_axis, math.rad(90))
+	Turn(thrust_R, x_axis, math.rad(90))
+
+	Turn(rad_L, x_axis, math.rad(180))
+	Turn(rad_R, x_axis, math.rad(180))
+	Move(rad_L, z_axis, -2)
+	Move(rad_R, z_axis, -2)
+
+	WingStart()
+	Hide(ball)
+	Hide(radiator_L)
+	Hide(radiator_R)
 	SetInitialBomberSettings()
 	StartThread(GG.TakeOffFuncs.TakeOffThread, takeoffHeight, SIG_TAKEOFF)
 	StartThread(GG.Script.SmokeUnit, unitID, smokePiece)
-	Hide(rearthrust)
-	Hide(wingthrust1)
-	Hide(wingthrust2)
-	Hide(flare1)
-	Hide(flare2)
-	Hide(drop)
+
 end
 
 function script.FireWeapon(num)
-	gun_1 = not gun_1
 	SetUnarmedAI()
 	OnAmmoChange(1)
 	Sleep(50)	-- delay before clearing attack order; else bomb loses target and fails to home
@@ -139,7 +231,7 @@ function script.AimWeapon(num)
 end
 
 function script.QueryWeapon(num)
-	return (gun_1 and flare1) or flare2
+	return drop
 end
 
 function script.BlockShot(num)
@@ -151,22 +243,24 @@ function script.Killed(recentDamage, maxHealth)
 	if not deathexplosiontriggered then
 		DeathExplosion()
 	end
+	local effect = SFX.FALL + SFX.SMOKE + SFX.FIRE
+	if severity > .50 then effect = effect + SFX.EXPLODE end
 	if severity <= .25 then
 		Explode(base, SFX.NONE)
-		Explode(wing1, SFX.NONE)
-		Explode(wing2, SFX.NONE)
+		Explode(wing_L, SFX.NONE)
+		Explode(wing_R, SFX.NONE)
 		return 1
 	elseif severity <= .50 or ((Spring.GetUnitMoveTypeData(unitID).aircraftState or "") == "crashing") then
-		Explode(wing1, SFX.FALL + SFX.SMOKE + SFX.FIRE)
-		Explode(wing2, SFX.FALL + SFX.SMOKE + SFX.FIRE)
+		Explode(wing_L, effect)
+		Explode(wing_R, effect)
 		return 1
 	elseif severity <= .75 then
-		Explode(wing1, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
-		Explode(wing2, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
+		Explode(wing1, effect)
+		Explode(wing2, effect)
 		return 2
 	else
-		Explode(wing1, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
-		Explode(wing2, SFX.FALL + SFX.SMOKE + SFX.FIRE + SFX.EXPLODE)
+		Explode(wing1, effect)
+		Explode(wing2, effect)
 		return 2
 	end
 end
