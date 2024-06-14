2b31c97ed6435fbdb51e3dcfdcb18a73a20b0203
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2024-06-13 23:33:56 -0400

    Fix a lot of uneven mex values

---
+++ LuaRules/Configs/MetalSpots/Geyser Plains BAR v1.2.lua
@@ -0,0 +1,34 @@
+local low = 3.0
+local high = 5.0
+
+return {
+	spots = {
+		{x = 3736, z = 3128, metal = high},
+		{x = 1400, z = 712, metal = low},
+		{x = 2856, z = 200, metal = low},
+		{x = 3000, z = 248, metal = low},
+		{x = 2104, z = 3960, metal = low},
+		{x = 2120, z = 2280, metal = low},
+		{x = 3768, z = 2280, metal = low},
+		{x = 1656, z = 1608, metal = high},
+		{x = 3624, z = 2136, metal = low},
+		{x = 280, z = 120, metal = low},
+		{x = 1576, z = 3880, metal = low},
+		{x = 456, z = 2120, metal = low},
+		{x = 1320, z = 3368, metal = low},
+		{x = 3608, z = 296, metal = low},
+		{x = 472, z = 2424, metal = low},
+		{x = 3688, z = 2968, metal = low},
+		{x = 136, z = 872, metal = low},
+		{x = 2440, z = 2744, metal = high},
+		{x = 3784, z = 1944, metal = low},
+		{x = 248, z = 744, metal = low},
+		{x = 2936, z = 1144, metal = high},
+		{x = 504, z = 3000, metal = low},
+		{x = 1160, z = 200, metal = low},
+		{x = 232, z = 1976, metal = low},
+		{x = 3832, z = 3848, metal = low},
+		{x = 3752, z = 408, metal = low},
+		{x = 1752, z = 152, metal = high},
+	}
+}
