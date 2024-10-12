1963cd8f9b7d1e86b26283787c9ad471109d70bf
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2024-10-12 06:43:58 -0400

    Add a give con button
    This allows you to transfer an idle constructor to an ally without one.

--- LuaUI/Widgets/game_con_notifier.lua
+++ LuaUI/Widgets/game_con_notifier.lua
@@ -1,7 +1,7 @@
 function widget:GetInfo()
 	return {
 		name      = "Con Notifier",
-		desc      = "Notifies when you're out of cons",
+		desc      = "Notifies when your team is out of cons and gives an easy global command bar fix.",
 		author    = "Shaman",
 		date      = "4/15/2021",
 		license   = "PD-0",
@@ -12,6 +12,65 @@ function widget:GetInfo()
 end
 
 local ColorToInColor
+
+local needsCons = {}
+
+local global_command_button
+
+local function IsPlayerAFK(playerID)
+	return spGetPlayerRulesParam(playerID, "lagmonitor_lagging") ~= nil
+end
+
+local function IsTeamAFK(teamID)
+	local playerList = Spring.GetPlayerList(teamID)
+	--Spring.Echo("Number of players: " .. tostring(#playerList))
+	if #playerList == 0 then return false end
+	for p = 1, #playerList do
+		local playerID = playerList[p]
+		if not spGetPlayerRulesParam(playerID, "lagmonitor_lagging") then
+			return false
+		end
+	end
+	return true
+end
+
+local function ShareUnits(playername, teamID)
+	if not teamID then
+		Spring.Echo('Con Notifier: Invalid team to share.')
+		return
+	end
+	if IsTeamAFK(teamID) then
+		Spring.Echo("game_message: Can't share units to an AFK team.")
+		return
+	end
+	local selcnt = Spring.GetSelectedUnitsCount()
+	if selcnt > 0 then
+		Spring.SendCommands("say a: I gave a constructor to "..playername..".") -- TODO: localize this.
+		Spring.ShareResources(teamID, "units")
+	else
+		Spring.Echo('Con Notifier: No units selected to share.')
+	end
+end
+
+local function OnButtonClick(teamID)
+	local teamLeader = select(2, Spring.GetTeamInfo(teamID))
+	local playerName = Spring.GetPlayerInfo(teamLeader)
+	local selection = WG.ConTracker.GetIdleCons()
+	local selected
+	local currentSelection = Spring.GetSelectedUnits()
+	if selection == nil or #selection == 0 then return end -- nothing idle!
+	for i = 1, #selection do
+		local unitDefID = Spring.GetUnitDefID()
+		local ud = UnitDefs[unitDefID]
+		if ud.customParams.level == nil and ud.customParams.dynamic_comm == nil and not ud.isFactory then
+			selected = selection[i]
+			Spring.SelectUnit(selected)
+			ShareUnits(playerTeam, playerName)
+			Spring.SelectUnitMap(currentSelection)
+			return
+		end
+	end
+end
 
 local function GetTeamInColor(teamID)
 	local teamColor = Spring.GetTeamColor(teamID)
@@ -33,17 +92,34 @@ local function GetTeamName(teamID)
 	end
 end
 
+local function DoTheThing()
+	local longestWaiter = 9999999999999999999999999
+	local pickedTeam = -1
+	for teamID, gameFrame in pairs(needsCons) do
+		if gameFrame < longestWaiter then
+			longestWaiter = gameFrame
+			pickedTeam = teamID
+		end
+	end
+	if teamID == -1 then return end
+	OnButtonClick(pickedTeam)
+end
 
 
 local function OnConStateChange(teamID, hasCons)
+	local frame = Spring.GetGameFrame()
+	if frame < 10 then return end
+	if IsTeamAFK(teamID) then return end
 	local name, squad = GetTeamName(teamID)
 	if hasCons then
+		needsCons[teamID] = nil
 		if squad then
 			Spring.Echo("game_message: " .. WG.Translate("interface", "has_cons_squad", {name = name}))
 		else
 			Spring.Echo("game_message: " .. WG.Translate("interface", "has_cons", {name = name}))
 		end
 	else
+		needsCons[teamID] = frame
 		if squad then
 			Spring.Echo("game_message: " .. WG.Translate("interface", "has_no_cons_squad", {name = name}))
 		else
@@ -52,9 +128,14 @@ local function OnConStateChange(teamID, hasCons)
 	end
 end
 
+local function OnLocaleChange()
+	global_command_button.tooltip = WG.Translate("interface", "give_con_button")
+end
+
 function widget:Initialize()
 	WG.ConTracker.Subscribe(OnConStateChange, "connotifier")
 	ColorToInColor = WG.Chili.color2incolor
+	global_command_button = WG.GlobalCommandBar.AddCommand("LuaUI/Images/Misc/no_cons.png", WG.Translate("interface", "give_con_button", DoTheThing)
 end
 
 function widget:ShutDown()
