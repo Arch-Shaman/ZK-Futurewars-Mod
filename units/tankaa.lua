8c9058c1ab7f82f783ff507136e1878becc7eb22
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2024-11-03 12:52:46 -0500

    Pestle
    * Range: 1040 -> 1200
    * Velocity: 900 -> 1300

--- units/tankaa.lua
+++ units/tankaa.lua
@@ -91,7 +91,7 @@ return {
 				impulseFactor           = 0.4,
 				interceptedByShieldType = 1,
 				noSelfDamage            = true,
-				range                   = 1040,
+				range                   = 1200,
 				reloadtime              = 1/3,
 				myGravity				= 0.03,
 				size                    = 8,
@@ -101,7 +101,7 @@ return {
 				soundStartVolume	    = 1,
 				turret                  = true,
 				weaponType              = "Cannon",
-				weaponVelocity          = 900,
+				weaponVelocity          = 1300,
 				--coverage = 2200,
 			},
 			TRITARY = {
