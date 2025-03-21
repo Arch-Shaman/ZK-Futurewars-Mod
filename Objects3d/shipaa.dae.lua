31fb40bebe249a5b324e1168baeb1bc5b8b575ec
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2025-03-20 21:56:38 -0400

    Undercurrent
    Reworked to be more alpha-like AA. Given shield to make it multirole in sea.
    
    Damage: 5.5x2 -> 20.1x8
    Aoe: 0 -> 20
    Cost: 500 -> 450
    Health: 2300 -> 3500
    Radar: 1040 -> 1800
    Los: 780 -> 850

---
+++ Objects3d/shipaa.dae.lua
@@ -0,0 +1,4 @@
+return {
+	tex1 = "aaship1.dds",
+	tex2 = "aaship3.dds",
+}
--- scripts/shipaa.lua
+++ scripts/shipaa.lua
@@ -4,10 +4,10 @@ include "pieceControl.lua"
 local base, wakel, waker, hull, radar = piece ('base', 'wakel', 'waker', 'hull', 'radar')
 local turret = {piece('fturret'), piece('bturret')}
 local barrels = {piece('fbarrels'), piece('bbarrels')}
-local flare = {piece('fflare'), piece('bflare')}
+local flare = {piece('fflarebl'),piece('fflarebr'),piece('fflaretl'),piece('fflaretr'), piece('bflarebl'), piece('bflarebr'), piece('bflaretl'),piece('bflaretr')}
 
 local SIG_Move = 1
-local SIG_Aim = {2, 4}
+local SIG_Aim = {2, 2, 2, 2, 4, 4, 4, 4}
 
 local stuns = {false, false, false}
 local disarmed = false
@@ -19,7 +19,7 @@ function script.setSFXoccupy(num)
 end
 
 local function RestoreAfterDelay()
-	SetSignalMask(SIG_Aim[1] + SIG_Aim[2])
+	SetSignalMask(SIG_Aim[1] + SIG_Aim[5])
 	Sleep (5000)
 	for i = 1, 2 do
 		Turn (turret[i], y_axis, 0, math.rad(30))
@@ -79,31 +79,49 @@ function script.Create()
 end
 
 function script.AimFromWeapon(id)
-	return barrels[id]
+	if id < 5 then
+		return barrels[1]
+	else
+		return barrels[2]
+	end
 end
 
 function script.QueryWeapon(id)
-	return barrels[id]
+	return flare[id]
 end
+
+local turretTurnSpeed = math.rad(500)
 
 function script.AimWeapon(id, heading, pitch)
 	Signal(SIG_Aim[id])
 	SetSignalMask(SIG_Aim[id])
-
+	local slowMult = (Spring.GetUnitRulesParam(unitID,"baseSpeedMult") or 1) -- TODO: Optimize
+	local wantedTurret, wantedBarrel
+	if id < 5 then
+		wantedTurret = turret[1]
+		wantedBarrel = barrels[1]
+	else
+		wantedTurret = turret[2]
+		wantedBarrel = barrels[2]
+	end
 	while disarmed do
 		Sleep(34)
 	end
-
-	local slowMult = (Spring.GetUnitRulesParam(unitID,"baseSpeedMult") or 1)
-	Turn (turret[id], y_axis, heading, math.rad(360 * slowMult))
-	Turn (barrels[id], x_axis, -pitch, math.rad(360 * slowMult))
-
-	WaitForTurn (turret[id], y_axis)
-	WaitForTurn (barrels[id], x_axis)
-
+	Turn (wantedTurret, y_axis, heading, turretTurnSpeed * slowMult)
+	Turn (wantedBarrel, x_axis, -pitch, turretTurnSpeed * slowMult)
+	WaitForTurn (wantedTurret, y_axis)
+	WaitForTurn (wantedBarrel, x_axis)
 	StartThread (RestoreAfterDelay)
-
 	return true
+end
+
+function script.BlockShot(num, targetID)
+	local radarCheck = targetID and GG.DontFireRadar_CheckBlock(unitID, targetID)
+	return radarCheck or not GG.BatteryManagement.CanFire(unitID, num)
+end
+
+function script.FireWeapon(num)
+	GG.BatteryManagement.WeaponFired(unitID, num)
 end
 
 local explodables = {turret[1], turret[2], barrels[1], barrels[2], radar}
