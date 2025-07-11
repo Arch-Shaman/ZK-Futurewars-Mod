f1908b9fd2d96cd3c81ac9d43c768143e417f7fb
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2025-07-11 13:31:38 -0400

    [Ignore] [Ban] mr0neoncat
    mr0neoncat has been banned for concern trolling (we do not wish to condone this behavior in our community as we must focus our efforts into improving the game) as well as attempting to defame the lead developer by claiming that I "do not like people playing base game". This behavior is unacceptable, and will not be tolerated. This ban will remain until a sincere apology is written.

--- LuaRules/Configs/bannedplayers.lua
+++ LuaRules/Configs/bannedplayers.lua
@@ -1,4 +1,5 @@
 local bannedPlayers = {
+	["mr0neoncat"] = "Concern trolling / attempting to defame the lead developer is not a good idea if you enjoy playing so much. https://discordapp.com/channels/278805140708786177/278805140708786177/1393273553893200004 . This gameside ban will remain until you provide a handwritten, sincere letter of apology to the lead developer.",
 } -- expects name = reason
 
 return bannedPlayers
