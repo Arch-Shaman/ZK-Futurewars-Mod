14a3b47bd819f846541010bf841cc9dc870cbd41
Shaman <48730089+Arch-Shaman@users.noreply.github.com>
2025-07-18 16:59:38 -0400

    [Ignore] Microoptimize framerounding

--- LuaRules/Utilities/framemath.lua
+++ LuaRules/Utilities/framemath.lua
@@ -1,5 +1,7 @@
 function Spring.Utilities.RoundToNearestFrame(amount) -- takes a number like 2.253 and rounds it to the nearest frame value.
-	return math.max(tonumber(string.format("%.0f", amount * 30)), 1) / 30
+	local val = tonumber(string.format("%.0f", amount * 30))
+	if val < 1 then val = 1 end
+	return val / 30
 end
 
 function Spring.Utilities.Round(amount, places)
