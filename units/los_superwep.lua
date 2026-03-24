315a2eaa2616d5c69efc2c27c02a1efd1f8057e3
garfild888 <93161381+garfild888@users.noreply.github.com>
2026-03-24 07:14:52 +0300

    Update los_superwep.lua

--- units/los_superwep.lua
+++ units/los_superwep.lua
@@ -23,7 +23,6 @@ return {
 			area_cloak_upkeep = 0,
 			area_cloak_radius = 50,
 			area_cloak_grow_rate = 500,
-			area_cloak_decloak_distance = 150,
 			completely_hidden = 1, -- for widget-senpai not to notice me >w<
 			singuimmune = 1,
 		},
