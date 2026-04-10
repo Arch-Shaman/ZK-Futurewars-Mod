52394d331403c8149ff67a304312a194b0093431
garfild888 <93161381+garfild888@users.noreply.github.com>
2026-04-10 08:27:45 +0300

    SW LOS visibility fix and some minor stuff (#698)
    * Fix ship AA hitbox
    
    * Oblivion and Surge
    
    Now use proper wreck models
    
    * Whirlwind
    
    Fixed incorrect damage to shields
    
    * Pulse
    
    [Ignore] Corrected description
    
    * Pulse
    
    [Ignore] Corrected description
    
    * Support Commander
    
    Now can equip shields after upgrading jammer or area cloak
    
    * Commander Morph UI
    
    Updated several icons
    
    * Commander Morph UI
    
    Even more icons
    
    * Commander Morph UI
    
    Changed size a bit
    
    * Updated UI elements
    
    Improved consistency
    
    * Commander Morph UI
    
    Added new Drone Lord icon
    
    * Geothermal Generator Fix
    
    Fixed incorrect placement of the "wheel" part
    
    * Chicken Rebalance Attempt
    
    Twice as many, but half the damage and HP
    
    * Fix for EMP damage
    
    Temporal fix for EMP damage not being applied.
    
    * Delete geo.dae
    
    New model is already included into base ZK. Removing bloat
    
    * Factory Description Update
    
    Updated some old/vanilla names to current ones
    
    * Add New Icons
    
    Added some icons
    
    * Update bomb.png
    
    * More Icons
    
    * Update los_superwep.lua
    
    Bootleg fix for gui_vertLineAid widget
    
    * Update los_superwep.lua
    
    * Name changes
    
    * Update dynamicCommander.lua
    
    Fix damage upgrades not being properly applied
    
    ---------
    
    Co-authored-by: Shaman <48730089+Arch-Shaman@users.noreply.github.com>

--- units/los_superwep.lua
+++ units/los_superwep.lua
@@ -19,6 +19,10 @@ return {
 		customParams          = {
 			dontcount         = 1,
 			dontkill          = 1,
+			area_cloak = 1,
+			area_cloak_upkeep = 0,
+			area_cloak_radius = 50,
+			area_cloak_grow_rate = 500,
 			completely_hidden = 1, -- for widget-senpai not to notice me >w<
 			singuimmune = 1,
 		},
